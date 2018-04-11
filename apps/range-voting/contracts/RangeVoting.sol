b4871686fa483bda3b034bc862ca9ebf61df5837
Arthur L Lunn <ArthurLLunn@gmail.com>
2018-04-11 19:13:17 +0200

    Fix docstring issues with leading underscores

--- apps/range-voting/contracts/RangeVoting.sol
+++ apps/range-voting/contracts/RangeVoting.sol
@@ -172,9 +172,9 @@ contract RangeVoting is IForwarder, AragonApp {
     * @notice `addCandidate` allows the `ADD_CANDIDATES_ROLE` to add candidates
     *         (or options) to the current vote. 
     * @param _voteId id for vote structure this 'ballot action' is connected to    
-    * @param metadata Any additional information about the candidate.
+    * @param _metadata Any additional information about the candidate.
     *        Base implementation does not use this parameter.
-    * @param description This is the string that will be displayed along the
+    * @param _description This is the string that will be displayed along the
     *        option when voting
     */
     function addCandidate(uint256 _voteId, bytes _metadata, string _description)
@@ -182,26 +182,27 @@ contract RangeVoting is IForwarder, AragonApp {
     {
         // Get vote and canddiate into storage
         Vote storage vote = votes[_voteId];
-        CandidateState storage candidate = vote.candidates[keccak256(description)];
+        CandidateState storage candidate = vote.candidates[keccak256(_description)];
         // Make sure that this candidate has not already been added
         require(candidate.added == false);
         // Set all data for the candidate
         candidate.added = true;
         candidate.keyArrayIndex = candidateKeys.length++;
         candidate.metadata = _metadata;
-        vote.candidateKeys[keyArrayIndex] = description;
+        vote.candidateKeys[keyArrayIndex] = _description;
     }
     
     /**
     * @notice `getCandidate` serves as a basic getter using the description key
     *         to return the struct data. 
-    * @param description The candidate key used when adding the candidate.
+    * @param _voteId id for vote structure this 'ballot action' is connected to    
+    * @param _description The candidate key used when adding the candidate.
     */
     function getCandidate(uint256 _voteId, string _description)
     external constant returns(bool, bytes, uint8, uint256)
     {
         Vote memory vote = votes[_voteId];
-        CandidateState memory candidate = vote.candidates[keccak256(description)];
+        CandidateState memory candidate = vote.candidates[keccak256(_description)];
         return(
             candidate.added,
             candidate.metadata,
