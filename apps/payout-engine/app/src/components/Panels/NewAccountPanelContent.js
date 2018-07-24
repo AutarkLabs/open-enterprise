5d6dd31cb366837da1eee76f4a142ad00c191908
deam <deamkh@gmail.com>
2018-07-24 22:40:34 +0200

    Minor changes

--- apps/payout-engine/app/src/components/Panels/NewAccountPanelContent.js
+++ apps/payout-engine/app/src/components/Panels/NewAccountPanelContent.js
@@ -1,101 +1,106 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import IconRemove from '../Icons/Remove'
+import React, { Component } from "react"
+import styled from "styled-components"
+import IconRemove from "../Icons/Remove"
 import {
-    Button,
-    Field,
-    IconAdd,
-    Text,
-    TextInput,
-    theme,
-    DropDown
-} from '@aragon/ui'
+  Button,
+  Field,
+  IconAdd,
+  Text,
+  TextInput,
+  theme,
+  DropDown,
+} from "@aragon/ui"
 
 const { accent, textSecondary, textTertiary } = theme
 
-const isIntegerString = (value) => /^[0-9]*$/.test(value)
+const isIntegerString = value => /^[0-9]*$/.test(value)
 
 class NewAccountPanel extends Component {
   state = {
-    items: ['ETH', 'ARA', 'GIV'],
+    items: ["ETH", "ARA", "GIV"],
     activeItem: 0,
-    title: '',
+    title: "",
     limit: {
       value: null,
-      label: ''
+      label: "",
     },
   }
 
-  titleChange = (e) => {
-      this.setState({ title: e.target.value })
+  titleChange = e => {
+    this.setState({ title: e.target.value })
   }
 
-  limitChange = (e) => {
+  limitChange = e => {
     if (isIntegerString(e.target.value)) {
-      const { items, activeItem } = this.state
       this.setState({
-        limit: {
-          value: e.target.value,
-          label: items[activeItem]
-        }
+        limit: e.target.value,
       })
     }
   }
 
   createAccount = () => {
     const { onCreateAccount, onClose } = this.props
-    onCreateAccount(this.state)
+    const { title, limit, items, activeItem } = this.state
+    onCreateAccount({
+      title,
+      limit: {
+        value: limit,
+        label: items[activeItem],
+      },
+    })
     this.state.limit = {
       value: null,
-      label: '',
+      label: "",
     }
-    this.state.title = ''
+    this.state.title = ""
     onClose()
   }
 
   render() {
-    const {
-        title,
-        limit,
-        items,
-    } = this.state
+    const { title, limit, items } = this.state
 
     return (
-        <div>
-          <Text size="xxlarge">New Account</Text>
-          <FieldTitle>Title<Required>*</Required></FieldTitle>
-          <FieldInput
-            onChange={this.titleChange}
-            placeholder="Enter title"
-            type="title"
-            value={title}
-          />
-          <FieldTitle>Limit<Required>*</Required></FieldTitle>
-          <FieldText>What sort of limits do you want to set per allocation vote?</FieldText>
-          <LimitInput
-            onChange={this.limitChange}
-            placeholder="e.g. 20"
-            type="limit"
-            value={isIntegerString(limit.value) ? limit.value : ''}
-          />
-          <DropDown
-            items={items}
-            active={this.state.activeItem}
-            onChange={(activeItem) => this.setState({ activeItem })}
-          />
-          <Button mode="strong" type="submit" wide onClick={this.createAccount} >
-            Create Account
-          </Button>
-        </div>
+      <div>
+        <Text size="xxlarge">New Account</Text>
+        <FieldTitle>
+          Title<Required>*</Required>
+        </FieldTitle>
+        <FieldInput
+          onChange={this.titleChange}
+          placeholder="Enter title"
+          type="title"
+          value={title}
+        />
+        <FieldTitle>
+          Limit<Required>*</Required>
+        </FieldTitle>
+        <FieldText>
+          What sort of limits do you want to set per allocation vote?
+        </FieldText>
+        <LimitInput
+          onChange={this.limitChange}
+          placeholder="e.g. 20"
+          type="limit"
+          value={isIntegerString(limit) ? limit : ""}
+        />
+        <DropDown
+          items={items}
+          active={this.state.activeItem}
+          onChange={activeItem => this.setState({ activeItem })}
+        />
+        <Button mode="strong" type="submit" wide onClick={this.createAccount}>
+          Create Account
+        </Button>
+      </div>
     )
   }
 }
 
 const FieldInput = styled.input`
   height: 40px;
-  border: 1px solid #E6E6E6;
+  border: 1px solid #e6e6e6;
   border-radius: 3px;
-  box-shadow: 0 4px 4px 0 rgba(0,0,0,0.03);
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.03);
   padding-left: 1rem;
   margin-bottom: 1rem;
 `
@@ -109,18 +114,18 @@ const LimitInput = FieldInput.extend`
 `
 
 const FieldTitle = styled.span`
-  color: #B3B3B3;
+  color: #b3b3b3;
   font-weight: bold;
   text-transform: lowercase;
   font-variant: small-caps;
-  color: #B3B3B3;
+  color: #b3b3b3;
   display: block;
 `
 
 const Required = styled.span`
-  margin-left: .5rem;
+  margin-left: 0.5rem;
   float: none;
-  color: #00CBE6;
+  color: #00cbe6;
 `
 
 const FieldText = styled.p`
