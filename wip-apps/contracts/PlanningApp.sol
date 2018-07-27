463d14af2998bbae518a477ba1412a1173d9e1bb
Otto G <ottodevs@gmail.com>
2018-07-27 17:57:24 +0200

    Discard old bare kit files unused anymore

--- wip-apps/contracts/PlanningApp.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.18;
-
-import "../node_modules/@aragon/os/contracts/apps/AragonApp.sol";
-
-contract PlanningApp is AragonApp {    
-    function initialize(string _name) public onlyInit {
-        initialized();
-    }
-}
--- wip-apps/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var PlanningApp = artifacts.require("./misc/PlanningApp.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(PlanningApp);
-};
