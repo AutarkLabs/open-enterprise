337868c21168bc3e444cb0994c8e5fa9bc169921
Otto G <ottodevs@gmail.com>
2018-08-16 04:14:05 +0200

    Configure solidity-coverage with travis, fix some details:
    - Travis config
    - Fix tests with ganache-cli.sh script
    - Fix npm scripts
    - Add solium linting npm script
    - Add gas reporter script
    - Add npx runner to some ganache-cli commands
    - Change test-helpers package name
    - Update truffle config to add the new options
    
    NOTE: Some code changes were borrowed from aragon os project

--- apps/allocations/truffle.js
+++ apps/allocations/truffle.js
@@ -1,8 +1,24 @@
 var path = require('path')
 
+const mochaGasSettings = {
+  reporter: 'eth-gas-reporter',
+  reporterOptions: {
+    currency: 'USD',
+    gasPrice: 3,
+  },
+}
+
+const mocha = process.env.GAS_REPORTER ? mochaGasSettings : {}
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, './build/contracts/'),
   networks: {
+    rpc: {
+      network_id: 15,
+      host: 'localhost',
+      port: 8545,
+      gas: 6.9e6,
+    },
     development: {
       host: 'localhost',
       port: 8545,
@@ -18,4 +34,6 @@ module.exports = {
       gasPrice: 0x01,
     },
   },
+  build: {},
+  mocha,
 }
