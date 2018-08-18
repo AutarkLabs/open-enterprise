3ac7e531e7d18c65e0a685b5398d72f5268f1e85
Otto G <ottodevs@gmail.com>
2018-08-18 21:40:39 +0200

    Enable lerna hoisting
    - This reduces the bootstrapped project size by moving to common deps top level node_modules (initially from ~2.74GB to ~600MB)
    - Package-lock files were regenerated
    - Dependencies sorted ascending
    - @aragon/apm updated to 2.0.1 to test HTTP provider feature

--- apps/shared/test-helpers/ganache-cli.sh
+++ apps/shared/test-helpers/ganache-cli.sh
@@ -15,7 +15,7 @@ testrpc_running() {
 
 start_testrpc() {
   if [ "$SOLIDITY_COVERAGE" = true ]; then
-    npx testrpc-sc -i 16 --gasLimit 0xfffffffffff --port "$testrpc_port"  > /dev/null &
+    ../../node_modules/.bin/testrpc-sc -i 16 --gasLimit 0xfffffffffff --port "$testrpc_port"  > /dev/null &
   elif [ "$TRUFFLE_TEST" = true ]; then
     npx ganache-cli -i 15 --gasLimit 50000000 --port "$testrpc_port" > /dev/null &
   elif [ "$START_KIT" = true ]; then
