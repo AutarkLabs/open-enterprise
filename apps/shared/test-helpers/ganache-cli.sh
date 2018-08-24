d71fd1f1d4cd1d231c592838d08cf46036adc6d7
Otto G <ottodevs@gmail.com>
2018-08-24 13:39:17 +0200

    Automated Kit working with multiple http providers with a bash script

--- apps/shared/test-helpers/ganache-cli.sh
+++ apps/shared/test-helpers/ganache-cli.sh
@@ -3,18 +3,18 @@
 # Exit script as soon as a command fails.
 set -o errexit
 
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
-     # While $SOURCE is a symlink, resolve it
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-     echo "$DIR/node_modules/.bin"
-}
+# get_script_dir () {
+#      SOURCE="${BASH_SOURCE[0]}"
+#      # While $SOURCE is a symlink, resolve it
+#      while [ -h "$SOURCE" ]; do
+#           DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#           SOURCE="$( readlink "$SOURCE" )"
+#           # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+#           [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+#      done
+#      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#      echo "$DIR/node_modules/.bin"
+# }
 
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
