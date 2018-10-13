472e20aa4c046f97e47ef7c6c4058a63c060076f
topocount <kevinsiegler54@gmail.com>
2018-10-13 16:49:32 -0700

    state now loads on a fresh reload. switching between apps still loses state

--- apps/allocations/app/script.js
+++ apps/allocations/app/script.js
@@ -3,42 +3,17 @@ import { first, of } from 'rxjs' // Make sure observables have .first
 import { combineLatest } from 'rxjs'
 import { empty } from 'rxjs/observable/empty'
 
-/***********************
- *                     *
- *  recursive retry    *
- *                     *
- ***********************/
-
-const retryEvery = (callback, initialRetryTimer = 1000, increaseFactor = 5) => {
-  const attempt = (retryTimer = initialRetryTimer) => {
-    // eslint-disable-next-line standard/no-callback-literal
-    callback(() => {
-      console.error(`Retrying in ${retryTimer / 1000}s...`)
-
-      // Exponentially backoff attempts
-      setTimeout(() => attempt(retryTimer * increaseFactor), retryTimer)
-    })
-  }
-  attempt()
-}
-
-
-
 const app = new Aragon()
 let appState
-retryEvery(retry => {
-  app
-    .events()
-    .subscribe(handleEvents, err => {
-      console.error(
-        'Could not start background script execution due to the contract not loading events:',
-        err
-      )
-      retry()
-    })
-})
+app.events().subscribe(handleEvents)
+
 app.state().subscribe( (state) => {
-  appState = state
+  console.log(
+    'entered state subscription:\n',
+    state
+  )
+  appState = state ? state : {accounts:[]}
+  //appState = state
 })
 
 
@@ -52,7 +27,6 @@ async function handleEvents(response){
   let nextState
   switch (response.event) {
   case 'NewAccount':
-    console.log('NewAccount event Fired')
     nextState = await newAccount(appState, response.returnValues)
     break
   }
@@ -66,7 +40,6 @@ async function newAccount(state, { accountId }) {
     data: { ...data, executed: true },
   })
   try {
-    console.log('Hi from inside NewAccount')
     let updatedState = await updateState(state, accountId, transform)
     return updatedState
   }
@@ -74,8 +47,6 @@ async function newAccount(state, { accountId }) {
     console.error(
       'updateState failed to return:',
       err,
-      'Here\'s what returned:'
-      //updatedState
     )
   }
 }
@@ -87,7 +58,7 @@ async function newAccount(state, { accountId }) {
  ***********************/
 
 function loadAccountData(accountId) {
-  console.log('Hi from inside loadAccountData')
+  console.log('loadAccountData entered')
   return new Promise(resolve => {
     combineLatest(app.call('getPayout', accountId)).subscribe(
       ([account, metadata]) => {
@@ -101,10 +72,9 @@ async function updateAccounts(accounts, accountId, transform) {
   const accountIndex = accounts.findIndex(
     account => account.accountId === accountId
   )
-  console.log('Hi from inside UpdateAccounts')
   if (accountIndex === -1) {
     // If we can't find it, load its data, perform the transformation, and concat
-    console.log('ooo account not found')
+    console.log('account not found: retrieving from chain')
     return accounts.concat(
       await transform({
         accountId,
@@ -120,7 +90,6 @@ async function updateAccounts(accounts, accountId, transform) {
 
 async function updateState(state, accountId, transform) {
   const { accounts = [] } = state
-  console.log('Hi from inside UpdateState')
   try {
     let newAccounts = await updateAccounts(accounts, accountId, transform)
     let newState = {...state, accounts: newAccounts}
