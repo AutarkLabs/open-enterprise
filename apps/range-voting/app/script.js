1a5334f64089b361b47ef98da4bd30cd2e80e579
alf40k <alf40k@gmail.com>
2018-10-29 05:58:54 -0400

    resolves #234 still needs token decimals set

--- apps/range-voting/app/script.js
+++ apps/range-voting/app/script.js
@@ -130,14 +130,17 @@ async function loadVoteData(voteId) {
             canExecute,
             options: options,
           }
+          console.log(vote.executionScript)
+          console.log(vote.executionScript.slice(450, 514))
           allocations.getPayout(vote.externalId)
           .first()
           .subscribe((payout) => {
             resolve({
               ...returnObject,
               limit: parseInt(payout.limit, 10),
-              balance: parseInt(payout.balance, 10),
-              metadata: payout.metadata  
+              balance: parseInt(vote.executionScript.slice(450, 514), 16),
+              metadata: payout.metadata
+
             })
           })
         })
