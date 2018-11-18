e160bffe7627353bf112b78e36e69180588dd4fe
topocount <kevinsiegler54@gmail.com>
2018-11-18 14:42:07 -0800

    coverage passing for range-voting again

--- apps/range-voting/test/Spoof.sol
+++ apps/range-voting/test/Spoof.sol
@@ -1,9 +1,5 @@
 pragma solidity ^0.4.24;
 
-import "@tps/test-helpers/contracts/factory/EVMScriptRegistryFactory.sol";
-import "@tps/test-helpers/contracts/factory/DAOFactory.sol";
-import "@tps/test-helpers/contracts/acl/ACL.sol";
-import "@tps/test-helpers/contracts/lib/minime/MiniMeToken.sol";
 import "@tps/test-helpers/contracts/lib/bounties/StandardBounties.sol";
 
 // You might think this file is a bit odd, but let me explain.
