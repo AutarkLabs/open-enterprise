1e9237791853e4eafd57d2d01ff2be1ba8f17855
Otto G <ottodevs@gmail.com>
2018-11-27 22:16:45 +0100

    Create the Popup launch function and customization helpers
    - The popup ref is tracked in the App state to manage its behavior
    - CLIENT_ID is the public planning-suite github application ID
    - Github applications are transferable

--- apps/projects/app/components/App/App.js
+++ apps/projects/app/components/App/App.js
@@ -10,6 +10,49 @@ import { NewProject } from '../Panel'
 
 const ASSETS_URL = 'aragon-ui-assets/'
 
+// TODO: let the user customize the github app on settings screen?
+const CLIENT_ID = 'd556542aa7a03e640409'
+const GITHUB_URI = 'https://github.com/login/oauth/authorize'
+
+// TODO: This should be dynamically set depending on the execution environment (dev, prod...)
+const REDIRECT_URI = 'http://localhost:3333'
+
+export const githubPopup = (popup = null) => {
+  // Checks to save some memory if the popup exists as a window object
+  if (popup === null || popup.closed) {
+    popup = window.open(
+      // TODO: Improve readability here: encode = (params: Object) => (JSON.stringify(params).replace(':', '=').trim())
+      // encode uurl params
+      // `${GITHUB_URI}?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`,
+      `${REDIRECT_URI}/?code=232r3423`,
+      'githubAuth',
+      // TODO: Improve readability here: encode = (fields: Object) => (JSON.stringify(fields).replace(':', '=').trim())
+      `scrollbars=no,toolbar=no,location=no,titlebar=no,directories=no,status=no,menubar=no, ${getPopupDimensions()}`
+    )
+  } else popup.focus()
+  return popup
+}
+
+const getPopupDimensions = () => {
+  const { width, height } = getPopupSize()
+  const { top, left } = getPopupOffset({ width, height })
+  return `width=${width},height=${height},top=${top},left=${left}`
+}
+
+const getPopupSize = () => {
+  return { width: 650, height: 850 }
+}
+
+const getPopupOffset = ({ width, height }) => {
+  const wLeft = window.screenLeft ? window.screenLeft : window.screenX
+  const wTop = window.screenTop ? window.screenTop : window.screenY
+
+  const left = wLeft + window.innerWidth / 2 - width / 2
+  const top = wTop + window.innerHeight / 2 - height / 2
+
+  return { top, left }
+}
+
 class App extends React.Component {
   static propTypes = {
     app: PropTypes.object.isRequired,
@@ -74,7 +117,7 @@ class App extends React.Component {
   }
 
   handleGithubSignIn = () => {
-    console.log('popup here')
+    this.setState(({ oldPopup }) => ({ popup: githubPopup(oldPopup) }))
   }
 
   render() {
