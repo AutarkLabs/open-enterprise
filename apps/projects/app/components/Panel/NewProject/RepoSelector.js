909f4e5bb0f776bd3ea85b011e0556e1cb7feffa
Otto G <ottodevs@gmail.com>
2018-11-29 16:54:43 +0100

    Complete the Repo Selector component at NewProject panel:
    - Added the same loading animation from the aragon wrapper to keep consistency
    - Create and conditionally render the RepoSelector component in NewProject side panel
    - Create the initial repositories graphql query
    - Add a TODO note for the STATUS enum in utils/github.js

---
+++ apps/projects/app/components/Panel/NewProject/RepoSelector.js
@@ -0,0 +1,227 @@
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+import { Button, Text, TextInput, theme } from '@aragon/ui'
+
+import ethereumLoadingAnimation from '../../../assets/svg/ethereum-loading.svg'
+import { CheckBox } from '../../Shared'
+class Repo extends React.Component {
+  state = {
+    repos: [],
+    filteredRepos: [],
+    filtered: false,
+  }
+
+  componentDidMount() {
+    if (this.props.data && this.props.data.viewer) {
+      this.setState({
+        repos: this.props.data.viewer.repositories.edges,
+      })
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data.viewer.repositories) {
+      this.setState({
+        repos: nextProps.data.viewer.repositories.edges,
+      })
+    }
+  }
+
+  searchRepos = e => {
+    const repos = this.state.repos.filter(repo => {
+      if (repo.node.nameWithOwner.indexOf(e.target.value) > -1) {
+        return repo
+      } else {
+        return null
+      }
+    })
+
+    this.setState({
+      filteredRepos: repos,
+      filtered: true,
+    })
+  }
+
+  render() {
+    const { repos, filteredRepos, filtered } = this.state
+
+    const visibleRepos = filtered ? filteredRepos : repos
+
+    const repositories =
+      repos.length > 0 ? (
+        visibleRepos.map((repo, i) => {
+          return (
+            <div key={i} style={{ display: 'flex', alignItems: 'baseline' }}>
+              <CheckBox
+                style={{ marginRight: '10.5px', width: '17px', height: '17px' }}
+              />
+              <Text.Block
+                size="xlarge"
+                color={theme.textSecondary}
+                style={{ lineHeight: '26px' }}
+              >
+                {repo.node.nameWithOwner}
+              </Text.Block>
+            </div>
+          )
+        })
+      ) : (
+        <Loading />
+      )
+
+    return (
+      <React.Fragment>
+        <div>
+          <Text style={{ marginTop: '16px' }} weight="bold">
+            Which repos do you want to add?
+          </Text>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <TextInput
+              type="search"
+              style={{ margin: '16px 0', flexShrink: '0' }}
+              placeholder="Search for a repo"
+              wide
+              onChange={this.searchRepos}
+            />
+            <div
+              style={{
+                flexGrow: '1',
+                overflowY: 'auto',
+                margin: '16px 0',
+                // Hack needed to make the scrollable list, since the whole SidePanel is a scrollable container
+                height: 'calc(100vh - 253px)',
+              }}
+            >
+              {repositories}
+            </div>
+            <Button mode="strong" wide style={{ flexShrink: '0' }}>
+              Finish
+            </Button>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+const RepoCard = styled.div`
+  border-bottom: 1px #d1d5da solid;
+  padding: 16px;
+  margin-bottom: 16px;
+`
+
+const SearchContainer = styled.div`
+  border-bottom: 1px solid #d1d5da;
+  padding: 16px 0 16px 0;
+`
+
+const SearchBox = styled.input`
+  min-height: 34px;
+  width: 300px;
+  font-size: 14px;
+  padding: 6px 8px;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-position: right 8px center;
+  border: 1px solid #d1d5da;
+  border-radius: 3px;
+  outline: none;
+  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+`
+
+const Date = styled.p`
+  font-size: 12px;
+  color: #586069;
+  margin-left: 10px;
+  margin-bottom: 0;
+`
+
+const InfoContainer = styled.div`
+  display: flex;
+`
+
+const Circle = styled.div`
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  background: #f1e05a;
+  margin-right: 5px;
+  top: 2px;
+  position: relative;
+`
+
+const RepoDescription = styled.p`
+  font-size: 14px;
+  color: #586069;
+  margin: 4px 0 10px 0;
+`
+
+const RepoLink = styled.a`
+  font-weight: 600;
+  color: #0366d6;
+  cursor: pointer;
+  font-size: 20px;
+`
+
+const RepoDetails = styled.span`
+  color: #586069;
+  font-size: 12px;
+  margin-bottom: 0;
+`
+
+const Icon = styled.i`
+  margin-left: 16px;
+`
+
+const ScrollWrapper = styled.div`
+  /* position: relative; */
+  /* z-index: 1; */
+  /* max-height: 40%; */
+  overflow: auto;
+`
+
+const LoadingAnimation = styled.img`
+  display: block;
+  margin-bottom: 32px;
+`
+
+const Loading = () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+      flexDirection: 'column',
+    }}
+  >
+    <LoadingAnimation src={ethereumLoadingAnimation} />
+    Loading repos...
+  </div>
+)
+
+export default graphql(gql`
+  query {
+    viewer {
+      repositories(
+        affiliations: [COLLABORATOR, ORGANIZATION_MEMBER, OWNER]
+        isFork: false
+        first: 50
+        orderBy: { field: NAME, direction: DESC }
+      ) {
+        edges {
+          node {
+            nameWithOwner
+          }
+        }
+      }
+    }
+  }
+`)(Repo)
