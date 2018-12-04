5058f09e02a119fc54c8c76cece90c5f6f9bdf45
Otto G <ottodevs@gmail.com>
2018-12-05 00:12:47 +0100

    Extract shared components to the Shared folder and set import paths

--- apps/projects/app/components/Shared/index.js
+++ apps/projects/app/components/Shared/index.js
@@ -1,2 +1,8 @@
 export { default as CheckBox } from './CheckBox'
+export { default as CheckButton } from './CheckButton'
 export { default as Title } from './Title'
+export { default as DropDown } from './DropDown'
+export { default as SearchBar } from './SearchBar'
+export { default as DropDownButton } from './DropDownButton'
+export { default as EmptyContent } from './EmptyContent'
+export * from './assets'
