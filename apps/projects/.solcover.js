1804c6f11495978cf2df3ea1ee7b209bd81ff122
Otto G <ottodevs@gmail.com>
2019-01-21 10:32:43 -0300

    Ignore migrations at projects app solcover configuration

--- apps/projects/.solcover.js
+++ apps/projects/.solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
   norpc: true,
+  skipFiles: ['misc/Migrations.sol'],
   // TODO: Change this hack when the feel to update solidity-coverage upstream
   // rsync is needed so symlinks are resolved on copy of lerna packages
   testCommand:
