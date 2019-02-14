7daf53218c0af317d7ea3cd645633a525bbf4ea9
rkzel <34452131+rkzel@users.noreply.github.com>
2019-02-14 23:43:15 +0100

    291 (#411)
    * Review Application Panel FE
    
    * changes requested
    
    * corrected application date and avatar link
    
    * fixed linting

--- apps/projects/app/components/App/App.js
+++ apps/projects/app/components/App/App.js
@@ -256,6 +256,18 @@ class App extends React.PureComponent {
     // this.closePanel()
   }
 
+  onReviewApplication = props => console.log('onReviewApplication', props)
+
+  reviewApplication = issue => {
+    this.setState((_prevState, _prevProps) => ({
+      panel: PANELS.ReviewApplication,
+      panelProps: {
+        issue,
+        onSubmit: this.onReviewApplication,
+      },
+    }))
+  }
+
   curateIssues = issues => {
     this.setState((_prevState, _prevProps) => ({
       panel: PANELS.NewIssueCuration,
@@ -350,6 +362,8 @@ class App extends React.PureComponent {
               onRequestAssignment={this.requestAssignment}
               activeIndex={activeIndex}
               changeActiveIndex={this.changeActiveIndex}
+
+              onReviewApplication={this.reviewApplication}
             />
 
             <PanelManager
