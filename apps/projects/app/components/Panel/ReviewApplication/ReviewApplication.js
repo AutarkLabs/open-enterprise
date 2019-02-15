5b54e53f920b1dadc21ed3d2423a7011f054c2ae
rkzel <34452131+rkzel@users.noreply.github.com>
2019-02-15 22:15:36 +0100

    descriptioninput trumps multiline text, panels headers changes (#425)

--- apps/projects/app/components/Panel/ReviewApplication/ReviewApplication.js
+++ apps/projects/app/components/Panel/ReviewApplication/ReviewApplication.js
@@ -6,13 +6,12 @@ import { formatDistance } from 'date-fns'
 import {
   Field,
   Text,
-  TextInput,
   Button,
   Info,
   SafeLink,
 } from '@aragon/ui'
 
-import { FormField, FieldTitle } from '../../Form'
+import { FormField, FieldTitle, DescriptionInput } from '../../Form'
 import { IconGitHub, CheckButton } from '../../Shared'
 
 // external data, all of it
@@ -96,13 +95,11 @@ class ReviewApplication extends React.Component {
         <FormField
           label="Feedback"
           input={
-            <TextInput.Multiline
+            <DescriptionInput
               name='feedback'
               rows={3}
-              style={{ resize: 'none', height: 'auto' }}
               onChange={this.changeField}
               placeholder="Do you have any feedback to provide the applicant?"
-              wide
             />
           }
         />
@@ -140,8 +137,10 @@ const IssueTitle = styled(Text)`
   color: #717171;
   font-size: 17px;
   font-weight: 300;
-  line-height: 38px;
+  line-height: 1.5;
+  margin-bottom: 10px;
 `
+
 const ReviewButton = styled(Button).attrs({
   mode: 'strong',
 })`
