ecea6deb29d0d866aefbcfef28cf72986ec5f1ab
Otto G <ottodevs@gmail.com>
2019-02-22 02:33:26 +0100

    Refactor MultiDropdown component fixing props and logic

--- apps/allocations/app/components/Form/Input/MultiDropdown.js
+++ apps/allocations/app/components/Form/Input/MultiDropdown.js
@@ -2,50 +2,43 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { DropDown } from '@aragon/ui'
 
-class MultiDropdown extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    entities: PropTypes.object,
-    value: PropTypes.array.isRequired,
-    index: PropTypes.number.isRequired,
-    activeItem: PropTypes.number.isRequired,
-    validator: PropTypes.func.isRequired,
-  }
-
-  onChangeInput = (index, items) => {
-    const { name, value, entities } = this.props
-    let newValue = {
+const MultiDropdown = ({
+  activeItem,
+  entities,
+  name,
+  onChange,
+  validator,
+  value,
+}) => {
+  const onChangeInput = index => {
+    const newValue = {
       addr: entities[index].addr,
-      index: index
+      index: index,
     }
-    if(this.props.validator(value, newValue.addr)) {
-      this.props.onChange({ target: { name: 'addressError', value: true} })
-    } else {
-      this.props.onChange({ target: { name: 'addressError', value: false} })
-    }
-    this.setState({ activeItem: index})
-    if(name === 'optionsInput'){
-      this.props.onChange({ target: { name: 'optionsInput', value: newValue } })
-    } else {
-      value[this.props.index] = newValue
-      this.props.onChange({ target: { name, value: value } })
-    }
+    const validation = validator(value, newValue.addr) ? true : false
+    const inputValue = name === 'optionsInput' ? newValue : value
+
+    // First onChange is for validator, second is for the input itself
+    onChange({ target: { name: 'addressError', value: validation } })
+    onChange({ target: { name: name, value: inputValue } })
   }
 
-  nameWrapper = name => <span style={{ whiteSpace: 'normal' }}>{name}</span>
+  const items = entities.map(entity => (
+    <span style={{ whiteSpace: 'normal' }}>{entity.data.name}</span>
+  ))
 
-  render() {
-    return (
-      <DropDown
-        items={this.props.entities.map(entity => this.nameWrapper(entity.data.name))}
-        active={this.props.activeItem}
-        onChange={this.onChangeInput}
-        wide={true}
-      />
-    )
-  }
+  return (
+    <DropDown items={items} active={activeItem} onChange={onChangeInput} wide />
+  )
 }
 
+MultiDropdown.propTypes = {
+  activeItem: PropTypes.number.isRequired,
+  entities: PropTypes.array,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  validator: PropTypes.func.isRequired,
+  value: PropTypes.array.isRequired,
+}
 
 export default MultiDropdown
