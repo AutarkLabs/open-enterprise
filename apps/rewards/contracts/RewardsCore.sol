458ca8a7b069007289ebf1da437ffea1f6582cbb
alf40k <alf40k@gmail.com>
2019-03-01 18:17:46 -0500

    Initial function signatures set for Rewards App

--- apps/rewards/contracts/RewardsCore.sol
+++ apps/rewards/contracts/RewardsCore.sol
@@ -1,14 +1,51 @@
 pragma solidity ^0.4.24;
 
+import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/contracts/common/IsContract.sol";
 
-contract RewardsCore {
-    function set(uint _value) public {
-        value = _value;
+
+contract RewardsCore is IsContract, AragonApp {
+
+    event RewardAdded(uint rewardId);
+    event RewardClaimed(uint rewardId);
+
+    bytes32 public constant ADD_REWARD_ROLE =  keccak256("ADD_REWARD_ROLE");
+
+    function initialize( Vault _vault)
+    external onlyInit // solium-disable-line visibility-first
+    {
+        initialized();
+
+        require(isContract(_vault), ERROR_VAULT_NOT_CONTRACT);
+
+        vault = _vault;
     }
 
-    function get() public constant returns (uint) {
-        return value;
+    function newReward(
+        bool isMerit,
+        address token,
+        uint amount,
+        uint duration,
+        uint occurances,
+        uint delay
+    ) external payable auth(ADD_REWARD_ROLE) returns (uint rewardId) {
+
     }
 
-    uint value;
+    function claimReward(uint rewardID) external {
+
+    }
+
+    function getReward(uint rewardID) external view returns(
+        bool isMerit,
+        address token,
+        uint amount,
+        uint duration,
+        uint occurances,
+        uint delay
+    )
+    {
+
+    }
+
 }
