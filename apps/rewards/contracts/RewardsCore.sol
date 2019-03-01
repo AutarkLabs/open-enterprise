8162e7fe75ff7c823e65e260b0f89f3808e07919
topocount <kevinsiegler54@gmail.com>
2019-03-01 13:06:27 -0800

    Reward token tests running

--- apps/rewards/contracts/RewardsCore.sol
+++ apps/rewards/contracts/RewardsCore.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.24;
 
+import "@aragon/os/contracts/apps/AragonApp.sol";
+
+import "@tps/test-helpers/contracts/lib/misc/Migrations.sol";
+
 
 contract RewardsCore {
     function set(uint _value) public {
