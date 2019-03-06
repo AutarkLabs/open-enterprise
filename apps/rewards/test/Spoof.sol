be2a4962fd485cf9b0a862d4a601090e7a45c955
alf40k <alf40k@gmail.com>
2019-03-06 18:09:20 -0500

    Trigger rebuild to test coverage

--- apps/rewards/test/Spoof.sol
+++ apps/rewards/test/Spoof.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
 
+import "@tps/test-helpers/contracts/factory/EVMScriptRegistryFactory.sol";
 import "@tps/test-helpers/contracts/factory/DAOFactory.sol";
 import "@tps/test-helpers/contracts/acl/ACL.sol";
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
