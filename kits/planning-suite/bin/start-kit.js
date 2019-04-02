1b355e359519f904da3a9cba08f51221f3d23411
Otto G <ottodevs@gmail.com>
2019-04-02 06:23:59 +0200

    Create initial deployment script

--- kits/planning-suite/bin/start-kit.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// const execute = require('child_process').execSync
-
-// const buildCommand = 'aragon run --kit PlanningSuite --kit-init autark'
-
-console.log('Starting Planning Suite Kit... 🚀')
-
-// execute(buildCommand, { stdio: 'inherit' })
