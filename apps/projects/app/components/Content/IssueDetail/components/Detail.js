84261dbaeca09d90f899815b842855fdd2b491ec
Yalda Mousavinia <ymousavinia@gmail.com>
2019-04-03 03:09:05 +0200

    Added styles for markdown

--- apps/projects/app/components/Content/IssueDetail/components/Detail.js
+++ apps/projects/app/components/Content/IssueDetail/components/Detail.js
@@ -69,6 +69,98 @@ const Wrapper = styled.div`
   height: 100%;
   padding: 10px;
 `
+
+const MarkdownWrapper = styled.div`
+  h1, h2, h3, h4, h5, h6 {
+  	font-weight: 700;
+  	line-height: 1.0;
+  	cursor: text;
+  	position: relative;
+  	margin: 1em 0 15px;
+  	padding: 0;
+  }
+  h1 {
+  	font-size: 2.5em;
+  	border-bottom: 1px solid ${theme.contentBorder};
+  }
+  h2 {
+  	font-size: 2em;
+  	border-bottom: 1px solid ${theme.contentBorder};
+  }
+  h3 {
+    font-size: 1.55em;
+  }
+  h4 {
+  	font-size: 1.2em;
+  }
+  h5 {
+  	font-size: 1em;
+  }
+  h6 {
+  	color: ${theme.textSecondary};
+  	font-size: 1em;
+  }
+  p, blockquote, table, pre {
+  	margin: 3px 0;
+  }
+  blockquote {
+    padding: 0 15px;
+    border-left: 4px solid ${theme.textTertiary};
+    color: ${theme.textTertiary}; }
+  blockquote > :first-child {
+    margin-top: 0; }
+  blockquote > :last-child {
+    margin-bottom: 10px; 
+  }
+  a {
+    color: ${theme.gradientStart};
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  a > code,
+  p > code {
+    background-color: ${theme.textSecondary};
+    border-radius: 3px;
+    padding: 0.2em 0.4em;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  tr {
+    border-top: 1px solid ${theme.contentBorder};
+  }
+  tr:nth-child(2n) {
+    background-color: #f8f8f8;
+  }
+  th,
+  td {
+    border: 1px solid ${theme.contentBorder};
+    padding: 6px 13px;
+  }
+  img {
+  	max-width: 100%;
+  }
+  pre {
+    margin: 0;
+    background-color: ${theme.mainBackground};
+    border-radius: 3px;
+    overflow: auto;
+    padding: 16px;
+  }
+  ul {
+  	padding-left: 30px; }
+  	li:last-of-type {
+  	padding-bottom: 1rem;
+  }
+  ol {
+  	padding-left: 30px;
+  	padding-bottom: 1rem; }
+  	ol li ul:first-of-type {
+    margin-top: 0px;
+  }
+`
 // todo: wrapper component for different sizes (changes padding mostly)
 
 // ...that 10px margin result in a 20px gap
@@ -351,7 +443,9 @@ const Detail = ({
           {workStatus && <SummaryTable {...summaryData} />}
           <FieldTitle>Description</FieldTitle>
           <Text.Block style={{ marginTop: '20px', marginBottom: '20px' }}>
-            {body ? renderHTML(marked(body)) : 'No description available'}
+            <MarkdownWrapper>
+            	{body ? renderHTML(marked(body)) : 'No description available'}
+        	  </MarkdownWrapper>
           </Text.Block>
           <Text size="small" color={theme.textTertiary}>
             {labels.totalCount
