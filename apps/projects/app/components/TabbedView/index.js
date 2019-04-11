e740f49427dbb89f7f02f7b887f5c38fffa83eac
Otto G <ottodevs@gmail.com>
2019-04-11 23:41:17 +0200

    Implement Aragon TabBar
    Changes involved:
    - Remove old custom TabBar component and files
    - Remove old custom Tab component and files
    - (*) TabbedView could be merged with TabContent an removed as well, since is not needed anymore, but involves additional changes to AppContent render()
    - Add missing propTypes and correct existing
    - Temporarily create a Wrap div to put a white bg on the TabBar

--- apps/projects/app/components/TabbedView/index.js
+++ apps/projects/app/components/TabbedView/index.js
@@ -1,4 +1,2 @@
-export { default as Tab } from './Tab'
-export { default as TabBar } from './TabBar'
 export { default as TabbedView } from './TabbedView'
 export { default as TabContent } from './TabContent'
