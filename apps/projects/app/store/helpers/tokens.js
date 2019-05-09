565a35df062b2feccfcc0b2199f2b25d45b6fa94
topocount <kevinsiegler54@gmail.com>
2019-05-09 14:05:30 -0700

    Resolve #894 : Add ETH from Vault to Projects

--- apps/projects/app/store/helpers/tokens.js
+++ apps/projects/app/store/helpers/tokens.js
@@ -2,27 +2,20 @@ import { app } from '../app'
 import tokenSymbolAbi from '../../abi/token-symbol.json'
 import tokenDecimalsAbi from '../../abi/token-decimal.json'
 
+const ETHER_TOKEN_FAKE_ADDRESS = '0x0000000000000000000000000000000000000000'
+
 const tokenAbi = [].concat(tokenDecimalsAbi, tokenSymbolAbi)
 
-const loadTokenBalance = (tokenAddress, vaultContract) => {
-  return vaultContract.balance(tokenAddress).toPromise()
-}
-
-const loadToken = async (tokenAddress, vaultContract) => {
-  const tokenContract = app.external(tokenAddress, tokenAbi)
-
-  const [ balance, decimals, symbol ] = await Promise.all([
-    loadTokenBalance(tokenAddress, vaultContract),
-    tokenContract.decimals().toPromise(),
-    tokenContract.symbol().toPromise(),
-  ])
-
-  return ({
-    addr: tokenAddress,
-    symbol: symbol,
-    decimals: decimals,
-    balance: balance,
-  })
+export const initializeTokens = async (state, vaultContract) => {
+  const nextState = {
+    ...state,
+    tokens: [{
+      addr: ETHER_TOKEN_FAKE_ADDRESS,
+      symbol: 'ETH',
+      decimals: '18',
+    }]
+  }
+  return await syncTokens(nextState, { token: ETHER_TOKEN_FAKE_ADDRESS }, vaultContract)
 }
 
 export const syncTokens = async (state, { token }, vaultContract) => {
@@ -48,4 +41,25 @@ export const syncTokens = async (state, { token }, vaultContract) => {
     console.error('[Projects script] syncSettings settings failed:', err)
     return state
   }
+}
+
+const loadTokenBalance = (tokenAddress, vaultContract) => {
+  return vaultContract.balance(tokenAddress).toPromise()
+}
+
+const loadToken = async (tokenAddress, vaultContract) => {
+  const tokenContract = app.external(tokenAddress, tokenAbi)
+
+  const [ balance, decimals, symbol ] = await Promise.all([
+    loadTokenBalance(tokenAddress, vaultContract),
+    tokenContract.decimals().toPromise(),
+    tokenContract.symbol().toPromise(),
+  ])
+
+  return ({
+    addr: tokenAddress,
+    symbol: symbol,
+    decimals: decimals,
+    balance: balance,
+  })
 }
