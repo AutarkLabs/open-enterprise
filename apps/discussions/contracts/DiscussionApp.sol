ff656f813d03b69ac5e9147960a4aa3460b13a1e
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-06-18 14:40:07 -0400

    implement discussions as a useless forwarder

--- apps/discussions/contracts/DiscussionApp.sol
+++ apps/discussions/contracts/DiscussionApp.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/lib/math/SafeMath.sol";
+import "@aragon/os/contracts/common/IForwarder.sol";
 
 
-contract DiscussionApp is AragonApp {
+contract DiscussionApp is IForwarder, AragonApp {
     using SafeMath for uint256;
 
     event Post(address indexed author, string postCid, string discussionId, uint postId, uint createdAt);
@@ -12,6 +13,7 @@ contract DiscussionApp is AragonApp {
     event Hide(address indexed author, string discussionId, uint postId, uint hiddenAt);
 
     bytes32 constant public DISCUSSION_POSTER_ROLE = keccak256("DISCUSSION_POSTER_ROLE");
+    string private constant ERROR_CAN_NOT_FORWARD = "DISCUSSIONS_CAN_NOT_FORWARD";
 
     struct DiscussionPost {
         address author;
@@ -57,5 +59,38 @@ contract DiscussionApp is AragonApp {
         post.revisionCids.push(post.postCid);
         post.postCid = revisedPostCid;
         emit Revise(msg.sender, revisedPostCid, discussionId, postId, post.createdAt, now);
+    }
+
+    // Forwarding fns
+
+    /**
+    * @notice Tells whether the Voting app is a forwarder or not
+    * @dev IForwarder interface conformance
+    * @return Always true
+    */
+    function isForwarder() external pure returns (bool) {
+        return true;
+    }
+
+    /**
+    * @notice Creates a vote to execute the desired action, and casts a support vote if possible
+    * @dev IForwarder interface conformance
+    * @param _evmScript Start vote with script
+    */
+    function forward(bytes _evmScript) public {
+        require(canForward(msg.sender, _evmScript), ERROR_CAN_NOT_FORWARD);
+        bytes memory input = new bytes(0); // TODO: Consider input for this
+        address[] memory blacklist = new address[](1);
+        runScript(_evmScript, input, blacklist);
+    }
+
+    /**
+    * @notice Tells whether `_sender` can forward actions or not
+    * @dev IForwarder interface conformance
+    * @param _sender Address of the account intending to forward an action
+    * @return True if the given address can create votes, false otherwise
+    */
+    function canForward(address _sender, bytes) public view returns (bool) {
+        return true;
     }
 }
