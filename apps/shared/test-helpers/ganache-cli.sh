23517d82a1567cde1a8cab0793a3c19878845abe
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-06-19 13:30:04 -0400

    add script for running TPS without a client

--- apps/shared/test-helpers/ganache-cli.sh
+++ apps/shared/test-helpers/ganache-cli.sh
@@ -39,6 +39,9 @@ start_testrpc() {
 	elif [ "$DEV" = true ]; then
 		aragon devchain --reset --port "$testrpc_port" &
 		lerna run dev --parallel --scope=@tps/apps-* &
+	elif [ "$NO_CLIENT" = true ]; then
+		aragon devchain --reset --port "$testrpc_port" &
+		lerna run dev --parallel --scope=@tps/apps-* &
 	fi
 
 	testrpc_pid=$!
@@ -66,6 +69,9 @@ elif [ "$START_KIT" = true ] || [ "$RESTART_KIT" = true ]; then
 	npm run publish:apps && npm run start:kit
 	result=$?
 elif [ "$DEV" = true ]; then
+	npm run publish:http && npm run start:kit
+	result=$?
+elif [ "$NO_CLIENT" = true ]; then
 	npm run publish:http && npm run start:kit
 	result=$?
 elif [ "$CYPRESS" = true ]; then
