5b0ea62fce784b441f2e4c5ee2fb58fde059b3e3
Kevin Siegler <kevinsiegler54@gmail.com>
2019-06-24 17:57:38 -0700

    update test script to use explicit truffle binary

--- apps/projects/utils/test.sh
+++ apps/projects/utils/test.sh
@@ -2,8 +2,9 @@
 sleep 5
 IFS=$'\n' 
 cd ../../shared/integrations/StandardBounties
-BOUNT_ADDR=($(truffle migrate --network development | grep "^0x[[:alnum:]]\{40\}"))
-#truffle migrate --network development
+BOUNT_ADDR=($(./node_modules/.bin/truffle migrate --network development | grep "^0x[[:alnum:]]\{40\}"))
+#./node_modules/.bin/truffle version
+#./node_modules/.bin/truffle migrate --network development
 cd ../../../apps/projects
 unset IFS
 echo "Bounties Addresses: ${BOUNT_ADDR[*]}"
