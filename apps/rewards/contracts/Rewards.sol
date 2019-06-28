76c5d298782e2bb0d69bdc294b6bed4801e37c89
Otto G <ottodevs@gmail.com>
2019-06-28 18:04:41 +0200

    Improve comments for functions - part 1

--- apps/rewards/contracts/Rewards.sol
+++ apps/rewards/contracts/Rewards.sol
@@ -61,10 +61,10 @@ contract Rewards is AragonApp {
     event RewardClaimed(uint256 rewardId); /// Emitted when a reward is claimed
 
     /**
-    * @notice Initialize Rewards app for Vault at `_vault`
-    * @dev Initializes the Rewards app
-    * @param _vault Address of the vault Rewards will rely on (non changeable)
-    */
+     * @notice Initialize Rewards app for Vault at `_vault`
+     * @dev Initializes the Rewards app, this is the Aragon custom constructor
+     * @param _vault Address of the vault Rewards will rely on (non changeable)
+     */
     function initialize(Vault _vault) external onlyInit {
         require(isContract(_vault), ERROR_VAULT);
         vault = _vault;
@@ -72,10 +72,11 @@ contract Rewards is AragonApp {
     }
 
     /**
-    * @dev This function allows a user to claim their reward (if one is available)
-    * @notice Claim my reward for #`_rewardID`
-    * @param _rewardID The ID of the reward
-    */
+     * @notice Claim my reward for #`_rewardID`
+     * @dev Allows a user to claim their reward (if one is available)
+     * @param _rewardID The ID of the reward
+     * @return rewardAmount calculated for that reward ID
+     */
     function claimReward(uint256 _rewardID) external returns (uint256) {
         Reward storage reward = rewards[_rewardID];
 
@@ -96,10 +97,33 @@ contract Rewards is AragonApp {
         return rewardAmount;
     }
 
+    /**
+     * @notice Get total rewards count
+     * @dev Gets the lenght of the rewards registry array
+     * @return rewardsLength the length of the rewards array
+     */
     function getRewardsLength() external view returns (uint256 rewardsLength) {
         rewardsLength = rewards.length;
     }
 
+    /**
+     * @notice Gets information for the reward with ID #`_rewardID`
+     * @dev Allows a user to get information about a specific reward
+     * @param _rewardID The ID of the reward
+     * @return description message for this reward
+     * @return isMerit true or false in case it is a dividend reward
+     * @return referenceToken used as reference to weight the reward
+     * @return rewardToken used to pay the reward
+     * @return amount for this reward
+     * @return startBlock when the reward went active
+     * @return endBlock when the reward period ended
+     * @return duration timestamp for the reward duration
+     * @return delay in seconds? in case the reward start was postponed
+     * @return rewardAmount which amount is available to claim?
+     * @return claimed was it claimed by the msg.sender?
+     * @return timeClaimed when it was claimed by the msg.sender
+     * @return creator the address of the reward creator
+     */
     function getReward(uint256 rewardID) external view returns (
         string description,
         bool isMerit,
@@ -132,6 +156,11 @@ contract Rewards is AragonApp {
         rewardAmount = calculateRewardAmount(reward);
     }
 
+    /**
+     * @notice Get total amount of `_token` claimed
+     * @dev Gets the amount of this token claimed
+     * @return totalAmountClaimed for that token
+     */
     function getTotalAmountClaimed(address _token)
     external view isInitialized returns (uint256)
     {
@@ -139,19 +168,20 @@ contract Rewards is AragonApp {
     }
 
     /**
-        * @dev This function creates a reward instance to be added to the rewards array. ID's
-        *      are assigned the new intance's index of that array
-        * @notice Create a new `_isMerit ? 'merit reward' : 'dividend'` of `@tokenAmount(_rewardToken, _amount)` for `_referenceToken.symbol(): string` holders (`_description`)
-        * @param _description description of the reward
-        * @param _isMerit Recurring dividend reward or one-off merit reward
-        * @param _referenceToken the token used to calculate reward distributions for each holder
-        * @param _rewardToken currency received as reward, accepts address 0 for ETH reward
-        * @param _amount the reward amount to be distributed
-        * @param _startBlock block in which token transactions will begin to be tracked
-        * @param _duration the time duration over which reference token earnings are calculated
-        * @param _occurrences the number of occurences of a dividend reward
-        * @param _delay the waiting time after the end of the period that the reward can be claimed
-    */
+     * @notice Create a new `_isMerit ? 'merit reward' : 'dividend'` of `@tokenAmount(_rewardToken, _amount)` for `_referenceToken.symbol(): string` holders (`_description`)
+     * @dev This function creates a reward instance to be added to the rewards array. ID's
+     *      are assigned the new intance's index of that array
+     * @param _description description of the reward
+     * @param _isMerit Recurring dividend reward or one-off merit reward
+     * @param _referenceToken the token used to calculate reward distributions for each holder
+     * @param _rewardToken currency received as reward, accepts address 0 for ETH reward
+     * @param _amount the reward amount to be distributed
+     * @param _startBlock block in which token transactions will begin to be tracked
+     * @param _duration the time duration over which reference token earnings are calculated
+     * @param _occurrences the number of occurences of a dividend reward
+     * @param _delay the waiting time after the end of the period that the reward can be claimed
+     * @return rewardId of the newly created Reward
+     */
     function newReward(
         string _description,
         bool _isMerit,
