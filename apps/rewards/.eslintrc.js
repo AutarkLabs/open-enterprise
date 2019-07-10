ce4d521633b6212f22eea78c262a02316c169860
Chad Ostrowski <hi@chadoh.com>
2019-07-10 11:30:39 -0400

    Rewards: update .eslintrc.js styles
    * no unnecessary quotes
    * prefer single quotes

--- apps/rewards/.eslintrc.js
+++ apps/rewards/.eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
+  env: {
+    browser: true,
+    es6: true
   },
-  "extends": "prettier/react",
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: 'prettier/react',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  "plugins": ["react"],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "react/no-typos": 1,
-    "semi": ["error", "never"],
-    "array-bracket-spacing": [
-      "error",
-      "always",
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'react/no-typos': 1,
+    semi: ['error', 'never'],
+    'array-bracket-spacing': [
+      'error',
+      'always',
       {
-        "objectsInArrays": false,
-        "arraysInArrays": false,
-        "singleValue": false
+        objectsInArrays: false,
+        arraysInArrays: false,
+        singleValue: false
       }
     ],
-    "object-curly-spacing": ["error", "always"]
+    'object-curly-spacing': ['error', 'always']
   }
 }
