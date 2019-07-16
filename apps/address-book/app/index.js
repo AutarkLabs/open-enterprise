1492fc9cb63e1ee46a53efdb72149b84977b2bda
Otto G <ottodevs@gmail.com>
2019-07-17 01:48:54 +0200

    Polish address book index.js and add TODO note

--- apps/address-book/app/index.js
+++ apps/address-book/app/index.js
@@ -1,5 +1,15 @@
+// eslint-disable-next-line import/no-unused-modules
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+/*
+ * TODO: Disabled for now to avoid spamming the console
+ * if (process.env.NODE_ENV !== 'production') {
+ *   const axe = require('react-axe')
+ *   axe(React, ReactDOM, 1000)
+ * }
+ */
+
 import { AragonApi } from '@aragon/api-react'
 import appStateReducer from './app-state-reducer'
 import App from './components/App/App'
