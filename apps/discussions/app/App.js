19a104ccfea5555cc77604429e2de4888c694d8f
Chad Ostrowski <hi@chadoh.com>
2019-08-22 15:08:52 -0400

    Discussions: add placeholder UI
    This UI shows up when you select Discussions in the sidebar. We have
    nothing to show for the Discussions app itself at this point in time,
    and now we explain that here.

--- apps/discussions/app/App.js
+++ apps/discussions/app/App.js
@@ -1,121 +1,41 @@
-import React, { useState } from 'react'
-import { useAragonApi } from '@aragon/api-react'
-import { Main, Button, TextInput } from '@aragon/ui'
+import React from 'react'
 import styled from 'styled-components'
+import { EmptyStateCard, GU, Header, Main, breakpoint } from '@aragon/ui'
+import nothingToSeeHere from './empty-state.png'
 
-import { ipfs } from './ipfs'
+const illustration = <img src={nothingToSeeHere} alt="" height="160" />
 
 function App() {
-  const { api, appState, connectedAccount } = useAragonApi()
-  const { syncing } = appState
-
-  const [text, setText] = useState('')
-  const [id, setId] = useState(-1)
-
-  const post = async () => {
-    const discussionPost = {
-      author: connectedAccount,
-      mentions: [],
-      type: 'Post',
-      text,
-    }
-
-    try {
-      if (id === -1) throw new Error('must enter iD')
-      const result = await ipfs.dag.put(discussionPost, {})
-      const cid = result.toBaseEncodedString()
-      await api.post(cid, id.toString()).toPromise()
-      setText('')
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  const revise = async () => {
-    let discussionPost = appState.discussions['123'][id]
-    discussionPost.text = text
-    discussionPost.revisions.push(discussionPost.postCid)
-    delete discussionPost.postCid
-    try {
-      const result = await ipfs.dag.put(discussionPost, {})
-      const cid = result.toBaseEncodedString()
-      await api.revise(cid, id, '123').toPromise()
-      setText('')
-      setId(-1)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  const hide = async () => {
-    try {
-      await api.hide(id, '123').toPromise()
-      setId(-1)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  console.log(appState)
   return (
     <Main>
-      <BaseLayout>
-        {syncing && <Syncing />}
-        <TextInput
-          placeholder="write a discussion"
-          value={text}
-          onChange={e => setText(e.target.value)}
+      <Header primary="Discussions Proof-of-Concept" />
+      <Content>
+        Other apps used by your organization can embed on-chain discussions,
+        thanks to the coordinating work this app does in the background.
+        Someday, you may see a dashboard here, showing all your ongoing
+        discussions.
+      </Content>
+      <Spacer>
+        <EmptyStateCard
+          text="This is a background app. Nothing to see here."
+          onActivate={() => <div />}
+          illustration={illustration}
         />
-        Enter post Id to hide or revise post
-        <TextInput.Number
-          placeholder="enter post ID to hide"
-          value={id}
-          onChange={e => setId(Number(e.target.value))}
-        />
-        <Buttons>
-          <Button mode="secondary" onClick={post}>
-            Make post
-          </Button>
-          <Button mode="secondary" onClick={revise}>
-            Revise post
-          </Button>
-          <Button mode="secondary" onClick={hide}>
-            Hide post
-          </Button>
-          <Button
-            mode="secondary"
-            onClick={async () => {
-              const events = await api.pastEvents('0', 'latest').toPromise()
-              console.log(events, 'VENTS')
-            }}
-          >
-            PAST EVENT
-          </Button>
-        </Buttons>
-      </BaseLayout>
+      </Spacer>
     </Main>
   )
 }
 
-const BaseLayout = styled.div`
+const Spacer = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
-  height: 100vh;
-  flex-direction: column;
+  margin-top: ${4 * GU}px;
 `
 
-const Buttons = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  grid-gap: 40px;
-  margin-top: 20px;
-`
-
-const Syncing = styled.div.attrs({ children: 'Syncing…' })`
-  position: absolute;
-  top: 15px;
-  right: 20px;
+const Content = styled.p`
+  padding-left: ${2 * GU}px;
+  padding-right: ${2 * GU}px;
+  ${breakpoint('medium', 'padding-left: 0; padding-right: 0;')}
 `
 
 export default App
