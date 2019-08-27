03da9d600d4c944fc917317e2e606decd9d0dcb3
Chad Ostrowski <hi@chadoh.com>
2019-08-27 15:34:56 -0400

    Projects: mv AppTitleButton
    Nothing else uses this anymore, since all other apps have been updated
    to new aragonUI

--- shared/ui/components/assets/components/index.js
+++ shared/ui/components/assets/components/index.js
@@ -7,6 +7,5 @@ export { default as IconDots } from './IconDots'
 export { default as IconArrow } from './IconArrow'
 export { default as IconMarkdown } from './IconMarkdown'
 export { default as MenuButton } from './MenuButton'
-export { default as AppTitleButton } from './AppTitleButton'
 export { default as AppTitle } from './AppTitle'
 
