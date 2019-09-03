1ceef86db7fde1782b56270711de84c96bbaffce
Chad Ostrowski <hi@chadoh.com>
2019-09-03 11:08:51 -0400

    Discussions: rm dependency on aragonUI from App.js
    This shouldn't be rendered anywhere, so there's no need for it to be
    pretty.

--- apps/discussions/app/App.js
+++ apps/discussions/app/App.js
@@ -1,41 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
-import { EmptyStateCard, GU, Header, Main, breakpoint } from '@aragon/ui'
-import nothingToSeeHere from './empty-state.png'
-
-const illustration = <img src={nothingToSeeHere} alt="" height="160" />
 
 function App() {
   return (
-    <Main>
-      <Header primary="Discussions Proof-of-Concept" />
-      <Content>
+    <React.Fragment>
+      <h1>Discussions Proof-of-Concept</h1>
+      <h2>This is a background app. Nothing to see here.</h2>
+      <p>
         Other apps used by your organization can embed on-chain discussions,
         thanks to the coordinating work this app does in the background.
         Someday, you may see a dashboard here, showing all your ongoing
         discussions.
-      </Content>
-      <Spacer>
-        <EmptyStateCard
-          text="This is a background app. Nothing to see here."
-          onActivate={() => <div />}
-          illustration={illustration}
-        />
-      </Spacer>
-    </Main>
+      </p>
+    </React.Fragment>
   )
 }
-
-const Spacer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: ${4 * GU}px;
-`
-
-const Content = styled.p`
-  padding-left: ${2 * GU}px;
-  padding-right: ${2 * GU}px;
-  ${breakpoint('medium', 'padding-left: 0; padding-right: 0;')}
-`
 
 export default App
