5972818cb9827290022ffed81dc63c0f8fedbbee
Emilio Silva Schlenker <e18r@disroot.org>
2019-09-04 20:03:45 -0500

    Rebase dev branch

--- apps/address-book/app/components/App/Entities.js
+++ apps/address-book/app/components/App/Entities.js
@@ -12,7 +12,6 @@ import {
 
 import { LocalIdentityBadge } from '../../../../../shared/identity'
 import { IconDelete } from '../../../../../shared/ui'
-import { Empty } from '../Card'
 
 // TODO: colors taken directly from Invision
 const ENTITY_TYPES = [
@@ -23,68 +22,63 @@ const ENTITY_TYPES = [
 
 const entitiesSort = (a, b) => a.data.name.toUpperCase() > b.data.name.toUpperCase() ? 1 : -1
 
-const Entities = ({ entities, onNewEntity, onRemoveEntity }) => {
+const Entities = ({ entities, onRemoveEntity }) => {
   const network = useNetwork()
   const removeEntity = address => () => onRemoveEntity(address)
 
-  if (entities.length === 0) {
-    return <Empty action={onNewEntity} />
-  } else {
-    return (
-      <DataView
-        mode="adaptive"
-        fields={[ 'Name', 'Address', 'Type' ]}
-        entries={
-          entities.sort(entitiesSort).map(({ data: { name, entryAddress, entryType } }) =>
-            [ name, entryAddress, entryType ]
-          )
-        }
+  return (
+    <DataView
+      mode="adaptive"
+      fields={[ 'Name', 'Address', 'Type' ]}
+      entries={
+        entities.sort(entitiesSort).map(({ data: { name, entryAddress, entryType } }) =>
+          [ name, entryAddress, entryType ]
+        )
+      }
 
-        renderEntry={([ name, entryAddress, entryType ]) => {
-          const typeRow = ENTITY_TYPES.filter(row => row.name === entryType)[0]
-          const values = [
-            <Text
-              key={entryAddress}
-              size="xlarge"
-              css="padding-bottom: 5px"
-            >
-              {name}
-            </Text>,
-            <LocalIdentityBadge
-              key={entryAddress}
-              networkType={network && network.type}
-              entity={entryAddress}
-              shorten={true}
-            />,
-            <Badge
-              key={entryAddress}
-              foreground={typeRow.fg}
-              background={typeRow.bg}
-              css="text-align: right"
-            >
-              {typeRow.name}
-            </Badge>
-          ]
-          return values
-        }}
+      renderEntry={([ name, entryAddress, entryType ]) => {
+        const typeRow = ENTITY_TYPES.filter(row => row.name === entryType)[0]
+        const values = [
+          <Text
+            key={entryAddress}
+            size="xlarge"
+            css="padding-bottom: 5px"
+          >
+            {name}
+          </Text>,
+          <LocalIdentityBadge
+            key={entryAddress}
+            networkType={network && network.type}
+            entity={entryAddress}
+            shorten={true}
+          />,
+          <Badge
+            key={entryAddress}
+            foreground={typeRow.fg}
+            background={typeRow.bg}
+            css="text-align: right"
+          >
+            {typeRow.name}
+          </Badge>
+        ]
+        return values
+      }}
 
-        renderEntryActions={([ , entryAddress ]) => (
-          <ContextMenu>
-            <ContextMenuItem onClick={removeEntity(entryAddress)}>
-              <IconDelete />
-              <span css="padding: 4px 8px 0px">Remove</span>
-            </ContextMenuItem>
-          </ContextMenu>
-        )}
-      />
-    )
-  }
+      renderEntryActions={([ , entryAddress ]) => (
+        <ContextMenu>
+          <ContextMenuItem onClick={removeEntity(entryAddress)}>
+            <IconDelete />
+            <span css="padding: 4px 8px 0px">Remove</span>
+          </ContextMenuItem>
+        </ContextMenu>
+      )}
+    />
+  )
 }
 
 Entities.propTypes = {
   // TODO: shape better
   entities: PropTypes.array.isRequired,
-  onNewEntity: PropTypes.func.isRequired,
   onRemoveEntity: PropTypes.func.isRequired,
 }
 
