c455c190a730a6f96a15b76b5fd6af9929d2318c
Otto G <ottodevs@gmail.com>
2019-09-06 20:20:31 +0200

    Revert isValuePct to pure function at DotVotingMock

--- apps/dot-voting/contracts/test/mocks/DotVotingMock.sol
+++ apps/dot-voting/contracts/test/mocks/DotVotingMock.sol
@@ -5,7 +5,7 @@ import "../../DotVoting.sol";
 
 contract DotVotingMock is DotVoting {
     // _isValuePct public wrapper
-    function isValuePct(uint256 _value, uint256 _total, uint256 _pct) external returns (bool) {
+    function isValuePct(uint256 _value, uint256 _total, uint256 _pct) external pure returns (bool) {
         return _isValuePct(_value, _total, _pct);
     }
 }
