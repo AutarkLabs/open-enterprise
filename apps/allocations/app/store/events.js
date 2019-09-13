14ec876303e1e61cb97cbe6a4be95f49bc9723af
Otto G <ottodevs@gmail.com>
2019-09-14 00:47:59 +0200

    Refactor allocations store to make it simpler

--- apps/allocations/app/store/events.js
+++ apps/allocations/app/store/events.js
@@ -1,42 +1,51 @@
-import { vaultLoadBalance } from './token'
-import { onFundedAccount, onNewAccount, onPayoutExecuted } from './account'
-import { addressesEqual } from '../utils/web3-utils'
+// import { vaultLoadBalance } from './token'
+import { updateAccounts } from './account'
+// import { addressesEqual } from '../utils/web3-utils'
 
-export const handleEvent = async (state, event, settings) => {
-  const { address: eventAddress, event: eventName, returnValues } = event
-  const { vault } = settings
-  const { accounts, payouts } = state
+const eventHandler = async eventData => {
+  const {
+    state,
+    event: { address, event, returnValues },
+    settings,
+  } = eventData
 
-  let nextAccounts, nextBoth
-  let nextState = { ...state }
-  if (addressesEqual(eventAddress, vault.address)) {
-    // Vault event
-    nextState = await vaultLoadBalance(nextState, event, settings)
-  } else {
-    switch (eventName) {
-    case 'FundAccount':
-      nextAccounts = await onFundedAccount(accounts, returnValues)
-      nextState.accounts = nextAccounts
-      break
-    case 'NewAccount':
-      nextAccounts = await onNewAccount(accounts, returnValues)
-      nextState.accounts = nextAccounts
-      break
-    case 'PayoutExecuted':
-      nextBoth = await onPayoutExecuted(payouts, accounts, returnValues)
-      nextState.accounts = nextBoth.accounts
-      nextState.payouts = nextBoth.payouts
-      break
-    case 'SetDistribution':
-      nextBoth = await onPayoutExecuted(payouts, accounts, returnValues)
-      nextState.accounts = nextBoth.accounts
-      nextState.payouts = nextBoth.payouts
-      break
-    default:
-      break
+  // const { vault } = settings
+  // const { accounts, payouts } = state
+
+  // let nextAccounts, nextBoth
+  // let nextState = { ...state }
+  // if (addressesEqual(eventAddress, vault.address)) {
+  // Vault event
+  // nextState = await vaultLoadBalance(nextState, event, settings)
+  // } else {
+  // console.log('Event received', eventName)
+
+  // Allocations event
+  switch (event) {
+  // case 'FundAccount':
+  //   nextAccounts = await onFundedAccount(accounts, returnValues)
+  //   nextState.accounts = nextAccounts
+  //   break
+  case 'NewAccount':
+    return {
+      ...state,
+      accounts: await updateAccounts(state.accounts, returnValues.accountId),
     }
+    // nextState.accounts = nextAccounts
+    // break
+    // case 'PayoutExecuted':
+    //   nextBoth = await onPayoutExecuted(payouts, accounts, returnValues)
+    //   nextState.accounts = nextBoth.accounts
+    //   nextState.payouts = nextBoth.payouts
+    //   break
+    // case 'SetDistribution':
+    //   nextBoth = await onPayoutExecuted(payouts, accounts, returnValues)
+    //   nextState.accounts = nextBoth.accounts
+    //   nextState.payouts = nextBoth.payouts
+    //   break
+  default:
+    return state
   }
-  // If nextAccounts were not generated
-  // then return each original array
-  return nextState
 }
+
+export default eventHandler
--- apps/allocations/app/store/init.js
+++ apps/allocations/app/store/init.js
@@ -1,81 +1,28 @@
-import { app, handleEvent } from './'
-import { first } from 'rxjs/operators'
+import {
+  getEthToken,
+  getNetwork,
+  getVault,
+  initializeTokens,
+  storeHandler,
+} from '../../../../shared/store-utils'
+import allocationsEventHandler from './events'
 
-import { initializeTokens } from './token'
-import vaultBalanceAbi from '../../../shared/json-abis/vault/vault-balance.json'
-import vaultGetInitializationBlockAbi from '../../../shared/json-abis/vault/vault-getinitializationblock.json'
-import vaultEventAbi from '../../../shared/json-abis/vault/vault-events.json'
+const initState = settings => async cachedState =>
+  await initializeTokens(cachedState, settings)
 
-const vaultAbi = [].concat(
-  vaultBalanceAbi,
-  vaultGetInitializationBlockAbi,
-  vaultEventAbi
-)
-
-const initialState = { accounts: [], payouts: [] }
-
-export const initialize = async (
-  vaultAddress,
-  ethAddress
-) => {
-  const vaultContract = app.external(vaultAddress, vaultAbi)
-  const network = await app
-    .network()
-    .pipe(first())
-    .toPromise()
-
-  return createStore({
-    network,
-    ethToken: {
-      address: ethAddress,
-    },
-    vault: {
-      address: vaultAddress,
-      contract: vaultContract,
-    },
-  })
-}
-
-const createStore = async settings => {
-  let vaultInitializationBlock
-
-  try {
-    vaultInitializationBlock = await settings.vault.contract
-      .getInitializationBlock()
-      .toPromise()
-  } catch (err) {
-    console.error(
-      '[allocations script] could not get attached vault\'s initialization block',
-      err
-    )
+const initialize = async () => {
+  const settings = {
+    ethToken: getEthToken(),
+    network: await getNetwork(),
+    vault: await getVault(),
   }
 
-  return app.store(
-    async (state, event) => {
-      const prevState = state || initialState
-      let nextState
-      try {
-        nextState = await handleEvent(prevState, event, settings)
-      } catch (err) {
-        console.error('[allocations script] initStore', event, err)
-      }
-      // always return the state even unmodified
-      return nextState
-    },
-    {
-      externals: [
-        {
-          contract: settings.vault.contract,
-          initializationBlock: vaultInitializationBlock
-        },
+  const storeOptions = {
+    externals: [settings.vault],
+    init: initState(settings),
+  }
 
-      ],
-      init: initState(settings)
-    }
-  )
+  return storeHandler(settings, allocationsEventHandler, storeOptions)
 }
 
-const initState = (settings) => async (cachedState) => {
-  const nextState = await initializeTokens(cachedState, settings)
-  return nextState
-}
+export default initialize
