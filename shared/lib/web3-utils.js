35925099656094d53d76be792b1df0d31395ae48
Otto G <ottodevs@gmail.com>
2019-09-20 17:56:11 +0200

    Restructure utils folder

--- shared/lib/web3-utils.js
+++ shared/lib/web3-utils.js
@@ -1,11 +1,13 @@
 import { toChecksumAddress } from 'web3-utils'
 
-// Check address equality without checksums
+// Check address equality with checksums
 export function addressesEqual(first, second) {
   first = first && toChecksumAddress(first)
   second = second && toChecksumAddress(second)
   return first === second
 }
 
+export const addressPattern = '(0x)?[0-9a-fA-F]{40}'
+
 // Re-export some web3-utils functions
-export { toUtf8 } from 'web3-utils'
+export { isAddress, toChecksumAddress, toUtf8 } from 'web3-utils'
