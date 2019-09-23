7233862743a43d2a9316b0ccf063bbe9837e4498
Emilio Silva Schlenker <e18r@disroot.org>
2019-09-23 10:11:48 -0500

    Deactivate Modal
    * Put it in its own file
    * Simplified the modal state hook on App.js

--- apps/allocations/app/index.js
+++ apps/allocations/app/index.js
@@ -2,16 +2,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AragonApi } from '@aragon/api-react'
-import { Main } from '@aragon/ui'
 import appStateReducer from './app-state-reducer'
 import App from './components/App/App'
 
 // TODO: Profile App with React.StrictMode, perf and why-did-you-update, apply memoization
 ReactDOM.render(
   <AragonApi reducer={appStateReducer}>
-    <Main>
-      <App />
-    </Main>
+    <App />
   </AragonApi>,
   document.querySelector('#allocations')
 )
