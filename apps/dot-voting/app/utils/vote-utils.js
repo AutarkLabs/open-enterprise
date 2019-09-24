313332bcc09b1176c96eb3158b2342080f9452a9
Chad Ostrowski <chad@autark.xyz>
2019-09-24 13:43:31 -0400

    Dot Voting/vote-utils: remove unused functions

--- apps/dot-voting/app/utils/vote-utils.js
+++ apps/dot-voting/app/utils/vote-utils.js
@@ -24,20 +24,3 @@ export const getTotalSupport = ({ options }) => {
   })
   return totalSupport
 }
-
-export function shortenAddress(address, charsLength = 4) {
-  const prefixLength = 2 // "0x"
-  if (!address) {
-    return ''
-  }
-  if (address.length < charsLength * 2 + prefixLength) {
-    return address
-  }
-  return (
-    address.slice(0, charsLength + prefixLength) +
-    '…' +
-    address.slice(-charsLength)
-  )
-}
-
-export { isAddress } from 'web3-utils'
