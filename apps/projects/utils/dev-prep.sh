77c08319d2a7959eae7a313f26efda8656476fe9
Chad Ostrowski <chad@autark.xyz>
2019-09-27 14:42:41 -0400

    Projects: exit dev-prep.sh if truffle not found

--- apps/projects/utils/dev-prep.sh
+++ apps/projects/utils/dev-prep.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sleep 5
 IFS=$'\n'
 cd ../../shared/integrations/StandardBounties
+
+if [[ ! -f node_modules/.bin/truffle ]] ; then
+  red='\e[1;31m%s\e[0m\n'
+  printf "$red" "  Cannot find `pwd`/node_modules/.bin/truffle – did you run \`npm install\`?"
+  printf "\n"
+  cd ../../../apps/projects
+  exit 1
+fi
+
 BOUNT_ADDR=($(node_modules/.bin/truffle migrate --network development | grep "^0x[[:alnum:]]\{40\}"))
 echo "Bounties Address: ${BOUNT_ADDR[*]}"
 BOUNT_ADDR=($(node_modules/.bin/truffle migrate --network development | grep "^0x[[:alnum:]]\{40\}"))
