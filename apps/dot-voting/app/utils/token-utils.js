7f7db844e1180a705f6257f9d7d71f779fad4016
Otto G <ottodevs@gmail.com>
2019-10-02 00:51:36 +0200

    Hack fix new allocations creation issue

--- apps/dot-voting/app/utils/token-utils.js
+++ apps/dot-voting/app/utils/token-utils.js
@@ -6,16 +6,18 @@ export const ETHER_TOKEN_FAKE_ADDRESS =
   '0x0000000000000000000000000000000000000000'
 
 export async function getTokenSymbol(app, address) {
-  // Symbol is optional; note that aragon.js doesn't return an error (only an falsey value) when
-  // getting this value fails
-  let token = app.external(address, tokenSymbolAbi)
-  let tokenSymbol = await token.symbol().toPromise()
-  if (tokenSymbol) {
-    return tokenSymbol
-  }
-  // Some tokens (e.g. DS-Token) use bytes32 as the return type for symbol().
-  token = app.external(address, tokenSymbolBytesAbi)
-  tokenSymbol = await token.symbol().toPromise()
+  // // Symbol is optional; note that aragon.js doesn't return an error (only an falsey value) when
+  // // getting this value fails
+  // let token = app.external(address, tokenSymbolAbi)
+  // let tokenSymbol = await token.symbol().toPromise()
+  // if (tokenSymbol) {
+  //   return tokenSymbol
+  // }
+  // // Some tokens (e.g. DS-Token) use bytes32 as the return type for symbol().
+  // token = app.external(address, tokenSymbolBytesAbi)
+  // tokenSymbol = await token.symbol().toPromise()
 
-  return tokenSymbol ? toUtf8(tokenSymbol) : null
+  // return tokenSymbol ? toUtf8(tokenSymbol) : null
+  // TODO: disabled for now as it is making new allocations creation fails with out of gas issue
+  return null
 }
