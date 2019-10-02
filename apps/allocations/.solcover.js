56810dc7ad1cf4f89cbdf2a90481f10bf252c56d
Peter Phillips <github@pmphillips.ca>
2019-10-02 10:18:30 -0700

    Added migrations folder and edited .solcover.js to the changes made to migrations and Imports.sol

--- apps/allocations/.solcover.js
+++ apps/allocations/.solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   norpc: true,
-  skipFiles: ['misc/Migrations.sol', 'test/TestImports.sol'],
+  skipFiles: ['test/Imports.sol'],
   // TODO: Change this hack when the feel to update solidity-coverage upstream
   // rsync is needed so symlinks are resolved on copy of lerna packages
   testCommand:
