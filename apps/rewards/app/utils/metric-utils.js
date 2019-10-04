f0889999dd5cab26026b73241e705f64168fdd55
Quazia <arthur@autark.xyz>
2019-10-04 11:33:55 -0400

    Fix one-time dividend time and my rewards

--- apps/rewards/app/utils/metric-utils.js
+++ apps/rewards/app/utils/metric-utils.js
@@ -61,3 +61,60 @@ const sumTotalRewards = (rewards, balances, convertRates, rewardFilter) => {
     else return balAcc
   },0)
 }
+
+export const calculateMyRewardsSummary = (rewards, balances, convertRates) => {
+  if (balances && convertRates) {
+    return [
+      calculateUnclaimedRewards(rewards, balances, convertRates),
+      calculateAllRewards(rewards, balances, convertRates),
+      calculateYTDUserRewards(rewards, balances, convertRates)
+    ]
+  }
+  else {
+    return Array(3).fill(0)
+  }
+}
+
+const calculateUnclaimedRewards = (rewards, balances, convertRates) => {
+  return sumUserRewards(
+    rewards,
+    balances,
+    convertRates,
+    (rew, bal) => !rew.claimed && rew.rewardToken === bal.address // rewardFilter
+  )
+}
+
+const calculateAllRewards = (rewards, balances, convertRates) => {
+  return sumUserRewards(
+    rewards,
+    balances,
+    convertRates,
+    (rew, bal) => rew.claimed && rew.rewardToken === bal.address, // RewardFilter
+  )
+}
+
+const calculateYTDUserRewards = (rewards, balances, convertRates) => {
+  const yearBeginning = new Date(new Date(Date.now()).getFullYear(), 0)
+  return sumUserRewards(
+    rewards,
+    balances,
+    convertRates,
+    (rew, bal) => rew.claimed && rew.rewardToken === bal.address && rew.endDate >= yearBeginning
+  )
+}
+
+const sumUserRewards = (rewards, balances, convertRates, rewardFilter) => {
+  return balances.reduce((balAcc, balance) => {
+    if (convertRates[balance.symbol]) {
+      return rewards.reduce((rewAcc,reward) => {
+        return (rewardFilter(reward, balance))
+          ?
+          BigNumber(reward.userRewardAmount).div(Math.pow(10, balance.decimals)).div(convertRates[balance.symbol]).plus(rewAcc)
+            .toNumber()
+          :
+          rewAcc
+      },0) + balAcc
+    }
+    else return balAcc
+  },0)
+}
