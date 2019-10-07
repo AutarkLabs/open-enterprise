dbb5358c103802e3967002e1a7ede2e0599c78e6
rkzel <rkz@hyperlogos.net>
2019-10-07 23:30:53 +0200

    post-review fixes - lint

--- apps/projects/app/utils/work-ratings.js
+++ apps/projects/app/utils/work-ratings.js
@@ -1,10 +1,10 @@
 const workRatings = [
-    'Select a Rating',
-    '1 - Unusable',
-    '2 - Needs Rework',
-    '3 - Acceptable',
-    '4 - Exceeds Expectations',
-    '5 - Excellent',
-  ]
+  'Select a Rating',
+  '1 - Unusable',
+  '2 - Needs Rework',
+  '3 - Acceptable',
+  '4 - Exceeds Expectations',
+  '5 - Excellent',
+]
 
 export default workRatings
