06607a9f1b4ae6b08ebf3c4f8269dcb5da29ed89
Peter Phillips <github@pmphillips.ca>
2019-10-08 18:25:59 -0700

    Changes to new-dao.js and create-dao.js to generate DAO but currently no frontend is published

--- templates/open-enterprise/temp/scripts/new-dao.js
+++ templates/open-enterprise/temp/scripts/new-dao.js
@@ -18,79 +18,44 @@ module.exports = async function newDao({
     votingSettings,
   } = settings
 
+  const { getEventArgument } = require('@aragon/test-helpers/events')
   const { getTemplateAddress } = require('../lib/ens')(web3, artifacts)
   const OpenEnterpriseTemplate = artifacts.require('OpenEnterpriseTemplate')
+  const Kernel = artifacts.require('Kernel')
 
   try {
-    // const template = OpenEnterpriseTemplate.at(await getTemplateAddress())
-    const template = OpenEnterpriseTemplate.at(
-      '0xee514cde999a30047d57c329f874f64b8a98ff46'
-    )
+    const template = OpenEnterpriseTemplate.at(await getTemplateAddress())
+    /*const template = OpenEnterpriseTemplate.at(
+      '0x67e4e0d558c55b2becb26df8dd84d88d193f4d85'
+    )*/
     console.log('template found at:', template.address)
-
-    // const tokenBase = await template.newToken(token.name, token.symbol)
-    // console.log('new token receipt', tokenBase.receipt)
-
-    const MEMBERS = members
-    const STAKES = stakes
-    const ONE_DAY = 60 * 60 * 24
-    const ONE_WEEK = ONE_DAY * 7
-    const THIRTY_DAYS = ONE_DAY * 30
-    const TOKEN_NAME = 'Autark Token'
-    const TOKEN_SYMBOL = 'AUT'
-    const VOTE_DURATION = ONE_WEEK
-    const SUPPORT_REQUIRED = 50e16
-    const MIN_ACCEPTANCE_QUORUM = 20e16
-    const VOTING_SETTINGS = [
-      SUPPORT_REQUIRED,
-      MIN_ACCEPTANCE_QUORUM,
-      VOTE_DURATION,
-    ]
-    const DOT_VOTING_SETTINGS = [
-      MIN_ACCEPTANCE_QUORUM,
-      SUPPORT_REQUIRED,
-      VOTE_DURATION,
-    ]
-    //   const DOT_VOTING_SETTINGS = [ SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION ]
 
     const daoID = randomId()
 
     console.log('daoId:', daoID)
 
     const baseDAO = await template.newTokenAndInstance(
-      TOKEN_NAME,
-      TOKEN_SYMBOL,
+      token.name,
+      token.symbol,
       daoID,
-      MEMBERS,
-      STAKES,
-      VOTING_SETTINGS,
+      members,
+      stakes,
+      votingSettings,
       0,
       { from: members[0] }
     )
 
-    console.log('test')
+    //console.log(baseDAO)
+    const dao = Kernel.at(getEventArgument(baseDAO, 'DeployDao', 'dao'))
+    console.log('DAO: ',dao.address)
 
     const baseOpenEnterprise = await template.newOpenEnterprise(
-      DOT_VOTING_SETTINGS,
+      dotVotingSettings,
       0,
-      false
+      false,
+      { from: members[0] }
     )
 
-    // const baseDAO = await template.newTokenAndInstance(
-    //   token.name,
-    //   token.symbol,
-    //   id,
-    //   members,
-    //   votingSettings,
-    //   financePeriod,
-    //   { from: members[0] }
-    // )
-
-    // const baseOpenEnterprise = await template.newOpenEnterprise(
-    //   dotVotingSettings,
-    //   allocationsPeriod,
-    //   useDiscussions
-    // )
     console.log(
       'created new dao with total cost',
       baseDAO.receipt.gasUsed + baseOpenEnterprise.receipt.gasUsed,
