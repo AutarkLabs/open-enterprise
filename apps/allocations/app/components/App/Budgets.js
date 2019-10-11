AutarkLabs/open-enterprise

commit a5eae4f7297528602893ca1c89ff13ee0cbc230f

Allocations/Budget Card: fix styling
Notes from Javi:

* Budget cards shouldn’t be clickable at this stage, since doing so
  doesn’t lead to anything
* There are three budget cards per row, while there should be four
* Change spacing between cards to 16px, not 32px

apps/allocations/app/components/App/Budgets.js
@@ -2,9 +2,8 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import { Viewport } from '@aragon/ui'
+import { GU } from '@aragon/ui'
 
-import { CARD_STRETCH_BREAKPOINT } from '../../utils/responsive'
 import { Budget } from '../Card'
 
 const Budgets = ({
@@ -15,28 +14,23 @@ const Budgets = ({
   onReactivate,
 }) => {
   return (
-    <Viewport>
-      {({ width }) => 
-        <StyledBudgets screenSize={width}>
-          {budgets.map(({ remaining, hasBudget, id, name, amount, token }) => (
-            <Budget
-              key={id}
-              id={id}
-              name={name}
-              amount={amount}
-              token={token}
-              remaining={remaining}
-              inactive={!hasBudget}
-              onNewAllocation={onNewAllocation}
-              onEdit={onEdit}
-              onDeactivate={onDeactivate}
-              onReactivate={onReactivate}
-              screenSize={width}
-            />
-          ))}
-        </StyledBudgets>
-      }
-    </Viewport>
+    <StyledBudgets>
+      {budgets.map(({ remaining, hasBudget, id, name, amount, token }) => (
+        <Budget
+          key={id}
+          id={id}
+          name={name}
+          amount={amount}
+          token={token}
+          remaining={remaining}
+          inactive={!hasBudget}
+          onNewAllocation={onNewAllocation}
+          onEdit={onEdit}
+          onDeactivate={onDeactivate}
+          onReactivate={onReactivate}
+        />
+      ))}
+    </StyledBudgets>
   )
 }
 
@@ -49,9 +43,9 @@ Budgets.propTypes = {
 }
 
 const StyledBudgets = styled.div`
-  display: flex;
-  flex-direction: ${props => props.screenSize < CARD_STRETCH_BREAKPOINT ? 'column' : 'row'};
-  flex-wrap: wrap;
+  display: grid;
+  grid-gap: ${2 * GU}px;
+  grid-template-columns: repeat(auto-fill, minmax(${27 * GU}px, 1fr));
 `
 
 export default Budgets

apps/allocations/app/components/Card/Budget.js
@@ -17,11 +17,6 @@ import {
   useTheme,
 } from '@aragon/ui'
 
-import {
-  BASE_CARD_WIDTH,
-  CARD_STRETCH_BREAKPOINT,
-} from '../../utils/responsive'
-
 const Budget = ({
   id,
   name,
@@ -33,7 +28,6 @@ const Budget = ({
   onEdit,
   onDeactivate,
   onReactivate,
-  screenSize
 }) => {
   const theme = useTheme()
 
@@ -56,7 +50,6 @@ const Budget = ({
     return (
       <Wrapper
         name={name}
-        screenSize={screenSize}
         theme={theme}
         menu={
           <ContextMenuItem onClick={reactivate}>
@@ -75,7 +68,6 @@ const Budget = ({
   return (
     <Wrapper
       name={name}
-      screenSize={screenSize}
       theme={theme}
       menu={
         <React.Fragment>
@@ -138,11 +130,10 @@ Budget.propTypes = {
   onEdit: PropTypes.func.isRequired,
   onDeactivate: PropTypes.func.isRequired,
   onReactivate: PropTypes.func.isRequired,
-  screenSize: PropTypes.number.isRequired,
 }
 
-const Wrapper = ({ children, name, screenSize, theme, menu }) => (
-  <StyledCard screenSize={screenSize} theme={theme}>
+const Wrapper = ({ children, name, theme, menu }) => (
+  <StyledCard theme={theme}>
     <MenuContainer>
       <ContextMenu>
         {menu}
@@ -160,30 +151,16 @@ const Wrapper = ({ children, name, screenSize, theme, menu }) => (
 Wrapper.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.string.isRequired,
-  screenSize: PropTypes.number.isRequired,
   theme: PropTypes.object.isRequired,
   menu: PropTypes.node.isRequired,
 }
 
 const StyledCard = styled(Card)`
-  display: flex;
-  margin-bottom: 2rem;
-  margin-right: ${props => props.screenSize < CARD_STRETCH_BREAKPOINT ? '0.6rem' : '2rem'};
   box-shadow: ${({ theme }) => '0 2px 4px ' + theme.border};
   border: 0;
-  flex-direction: column;
-  justify-content: flex-start;
   padding: 12px;
   height: 264px;
-  width: ${props =>
-    props.screenSize < CARD_STRETCH_BREAKPOINT
-      ? '100%'
-      : BASE_CARD_WIDTH + 'px'};
-  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
-  :hover {
-    cursor: pointer;
-    box-shadow: 0 9px 10px 0 rgba(101, 148, 170, 0.1);
-  }
+  width: auto;
 `
 
 const MenuContainer = styled.div`

apps/allocations/app/utils/responsive.js
@@ -1,5 +1,0 @@
-const REM_SIZE = parseFloat(getComputedStyle(document.documentElement).fontSize)
-const BASE_CARD_WIDTH = 264
-const CARD_STRETCH_BREAKPOINT = 2 * BASE_CARD_WIDTH + 2 * 2 * REM_SIZE + 2 * 2 * REM_SIZE
-
-export { BASE_CARD_WIDTH, CARD_STRETCH_BREAKPOINT }

commit 4214b52fdab538a631a8ff9ef8957dbb0b481b47

Allocations: add bottom margin to Budgets list
Fixes a style regression introduced in a5eae4f7297528602893ca1c89ff13ee0cbc230f

apps/allocations/app/components/App/Budgets.js
@@ -46,6 +46,7 @@ const StyledBudgets = styled.div`
   display: grid;
   grid-gap: ${2 * GU}px;
   grid-template-columns: repeat(auto-fill, minmax(${27 * GU}px, 1fr));
+  margin-bottom: ${2 * GU}px;
 `
 
 export default Budgets