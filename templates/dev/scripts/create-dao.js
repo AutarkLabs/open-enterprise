bc8727db61627bdea2d7825ac4a15c6092868762
Otto G <ottodevs@gmail.com>
2019-10-14 17:11:05 +0900

    Create and use dev template with flexible permissions

--- templates/dev/scripts/create-dao.js
+++ templates/dev/scripts/create-dao.js
@@ -1,11 +1,13 @@
 /* global artifacts, web3 */
 const { randomId } = require('@aragon/templates-shared/helpers/aragonId')
-const newDAO = require('../temp/scripts/new-dao')
+const newDAO = require('./new-dao')
 
 
 const VOTE_DURATION = 60 // seconds
-const SUPPORT_REQUIRED = 50e16 // 0 = 0%; 50e16 = 50%
-const MIN_ACCEPTANCE_QUORUM = 20e16 // 20e16 = 20%
+// TODO: this is min quorum in dot-voting? it does not accept zero, but 1e-16
+const SUPPORT_REQUIRED = 1 // 0 = 0%; 50e16 = 50%
+// this accepts zero in dot voting
+const MIN_ACCEPTANCE_QUORUM = 0 // 20e16 = 20%
 
 // define template params
 const settings = {
@@ -19,10 +21,10 @@ const settings = {
     '0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb',
   ],
   stakes: [
-    '1000000000000000000',
+    '10000000000000000000',
     '1000000000000000000',
   ],
-  token: { name: 'Autark Coin', symbol: 'AUT' },
+  token: { name: 'Autark DEV Coin', symbol: 'AUTD' },
   useDiscussions: true,
   // The order is important for now. TODO: make it an object instead
   votingSettings: [ SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION ],
---
+++ templates/dev/scripts/new-dao.js
@@ -0,0 +1,62 @@
+module.exports = async function newDao({
+  artifacts,
+  callback,
+  settings,
+  web3,
+}) {
+  const {
+    allocationsPeriod,
+    dotVotingSettings,
+    financePeriod,
+    id,
+    members,
+    stakes,
+    token,
+    useDiscussions,
+    votingSettings,
+  } = settings
+
+  
+
+  const { getEventArgument } = require('@aragon/test-helpers/events')
+  const { getTemplateAddress } = require('../../open-enterprise/temp/lib/ens')(web3, artifacts)
+  // TODO: Allow using DevTemplate or OpenEnterpriseTemplate with an env variable
+  const OpenEnterpriseTemplate = artifacts.require('DevTemplate')
+  const Kernel = artifacts.require('Kernel')
+
+  try {
+    const template = OpenEnterpriseTemplate.at(await getTemplateAddress())
+    
+    const baseDAO = await template.newTokenAndInstance(
+      token.name,
+      token.symbol,
+      'dev-' + id,
+      members,
+      stakes,
+      votingSettings,
+      0,
+      { from: members[0] }
+    )
+      
+    const dao = Kernel.at(getEventArgument(baseDAO, 'DeployDao', 'dao'))
+    const baseOpenEnterprise = await template.newOpenEnterprise(
+      dotVotingSettings,
+      0,
+      false,
+      { from: members[0] }
+    )
+
+    console.log('🤓 Template found at:', template.address)
+    console.log(
+      '🚀 Created new DEV dao at address:',
+      dao.address,
+      '\n⛽️ Total gas cost for creation:',
+      baseDAO.receipt.gasUsed + baseOpenEnterprise.receipt.gasUsed, 'gas',
+      '\n🌐 You can access it at:',
+      `http://localhost:8080/ipfs/QmVptozeYf3XxqHfvMjofCkZsYSqi6YuvHFLMc83SECbNw/#/dev-${id}`
+    )
+    callback()
+  } catch (e) {
+    callback(e)
+  }
+}
