16c42bb9e9f76a6563fab2cc12b991528b08a077
rkzel <rkz@hyperlogos.net>
2019-11-15 13:14:34 +0100

    applyFilters has many parameters, and now they are named
    Also, 'filters' are now called 'activeFilters' eveywhere, for clarity.

--- apps/projects/app/components/Content/applyFilters.js
+++ apps/projects/app/components/Content/applyFilters.js
@@ -1,21 +1,21 @@
-export const applyFilters = (issues, textFilter, filters, bountyIssues) => {
+export const applyFilters = ({ issues, textFilter, activeFilters, bountyIssues }) => {
   const bountyIssueObj = {}
   bountyIssues.forEach(issue => {
     bountyIssueObj[issue.issueNumber] = issue.data.workStatus
   })
 
   const issuesByProject = issues.filter(issue => {
-    if (Object.keys(filters.projects).length === 0) return true
-    if (Object.keys(filters.projects).indexOf(issue.repository.id) !== -1)
+    if (Object.keys(activeFilters.projects).length === 0) return true
+    if (Object.keys(activeFilters.projects).indexOf(issue.repository.id) !== -1)
       return true
     return false
   })
 
   const issuesByLabel = issuesByProject.filter(issue => {
     // if there are no labels to filter by, pass all
-    if (Object.keys(filters.labels).length === 0) return true
+    if (Object.keys(activeFilters.labels).length === 0) return true
     // if labelless issues are allowed, let them pass
-    if ('labelless' in filters.labels && issue.labels.totalCount === 0)
+    if ('labelless' in activeFilters.labels && issue.labels.totalCount === 0)
       return true
     // otherwise, fail all issues without labels
     if (issue.labels.totalCount === 0) return false
@@ -23,7 +23,7 @@ export const applyFilters = (issues, textFilter, filters, bountyIssues) => {
     const labelsIds = issue.labels.edges.map(label => label.node.id)
 
     if (
-      Object.keys(filters.labels).filter(id => labelsIds.indexOf(id) !== -1)
+      Object.keys(activeFilters.labels).filter(id => labelsIds.indexOf(id) !== -1)
         .length > 0
     )
       return true
@@ -31,29 +31,29 @@ export const applyFilters = (issues, textFilter, filters, bountyIssues) => {
   })
 
   const issuesByMilestone = issuesByLabel.filter(issue => {
-    // if there are no MS filters, all issues pass
-    if (Object.keys(filters.milestones).length === 0) return true
+    // if there are no MS activeFilters, all issues pass
+    if (Object.keys(activeFilters.milestones).length === 0) return true
     // should issues without milestones pass?
-    if ('milestoneless' in filters.milestones && issue.milestone === null)
+    if ('milestoneless' in activeFilters.milestones && issue.milestone === null)
       return true
     // if issues without milestones should not pass, they are rejected below
     if (issue.milestone === null) return false
-    if (Object.keys(filters.milestones).indexOf(issue.milestone.id) !== -1)
+    if (Object.keys(activeFilters.milestones).indexOf(issue.milestone.id) !== -1)
       return true
     return false
   })
 
   const issuesByStatus = issuesByMilestone.filter(issue => {
-    // if there are no Status filters, all issues pass
-    if (Object.keys(filters.statuses).length === 0) return true
+    // if there are no Status activeFilters, all issues pass
+    if (Object.keys(activeFilters.statuses).length === 0) return true
     // should bountyless issues pass?
     const status = bountyIssueObj[issue.number]
       ? bountyIssueObj[issue.number]
       : 'not-funded'
     // if we look for all funded issues, regardless of stage...
     let filterPass =
-      status in filters.statuses ||
-      ('all-funded' in filters.statuses && status !== 'not-funded')
+      status in activeFilters.statuses ||
+      ('all-funded' in activeFilters.statuses && status !== 'not-funded')
         ? true
         : false
     // ...or at specific stages
