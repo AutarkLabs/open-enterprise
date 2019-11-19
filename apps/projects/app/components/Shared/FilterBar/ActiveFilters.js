8cecb4271f5060fac54692f1c0e7ac452426d255
rkzel <rkz@hyperlogos.net>
2019-11-20 00:21:44 +0100

    redesigned component

--- apps/projects/app/components/Shared/FilterBar/ActiveFilters.js
+++ apps/projects/app/components/Shared/FilterBar/ActiveFilters.js
@@ -1,90 +1,29 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Button, Text, useTheme } from '@aragon/ui'
 
 import FilterTile from './FilterTile'
-import { prepareFilters } from '../Shared/FilterBar'
-import { issueShape } from '../../utils/shapes.js'
+import { useIssueFilters } from '../../../context/IssueFilters'
 
-const Filters = ({ filters, issues, bountyIssues, disableFilter, disableAllFilters, style }) => {
+const ActiveFilters = () => {
   const theme = useTheme()
-
-  const generateFilterNamesAndPaths = (filterInformation, type, textFieldToUse) => {
-    const appliedFilters = {}
-    Object.keys(filterInformation[type]).map(id => {
-      const filterApplied = id in filters[type]
-      const filterName = filterInformation[type][id][textFieldToUse]
-      if (filterApplied) {
-        appliedFilters[filterName] = [ type, id ]
-      }
-    })
-    return appliedFilters
-  }
-
-  /*
-    creates one object that looks like this:
-    {
-      filter1Name: [path, to, filter1, on, parent],
-      filter2Name: [path, to, filter2, on, parent],
-    }
-
-    to make it easier to deselect filters from this view without multiple state objects
-  */
-  const calculateFilters = () => {
-    const filterInformation = prepareFilters(issues, bountyIssues)
-
-    const projectBasedFilters = generateFilterNamesAndPaths(
-      filterInformation,
-      'projects',
-      'name'
-    )
-    const labelBasedFilters = generateFilterNamesAndPaths(
-      filterInformation,
-      'labels',
-      'name'
-    )
-    const milestoneBasedFilters = generateFilterNamesAndPaths(
-      filterInformation,
-      'milestones',
-      'title'
-    )
-    const statusBasedFilters = generateFilterNamesAndPaths(
-      filterInformation,
-      'statuses',
-      'name'
-    )
-
-    return {
-      ...projectBasedFilters,
-      ...labelBasedFilters,
-      ...milestoneBasedFilters,
-      ...statusBasedFilters,
-    }
-  }
-
-  const filterAliases = calculateFilters()
-
-  if (Object.keys(filterAliases).length === 0) return null
+  const { activeFilters, activeFiltersCount, availableFilters, resetFilters, toggleFilter } = useIssueFilters()
 
   return (
-    <Wrap style={style}>
-      {Object.keys(filterAliases).map(alias => {
-        const pathToDisableFilter = filterAliases[alias]
-        return (
+    <Wrap>
+      {Object.keys(activeFilters).map(type =>
+        Object.keys(activeFilters[type]).map(id =>
           <FilterTile
-            key={pathToDisableFilter.join('')}
-            text={alias}
-            disableFilter={() =>
-              disableFilter(pathToDisableFilter)
-            }
+            key={id}
+            text={availableFilters[type][id].name}
+            disableFilter={() => toggleFilter(type, id)}
           />
         )
-      })}
-      {Object.keys(filterAliases).length > 0 && (
+      )}
+      {activeFiltersCount > 0 && (
         <Button
           size="mini"
-          onClick={disableAllFilters}
+          onClick={resetFilters}
           css={`
             margin-left: 8px;
             border: 0;
@@ -107,33 +46,4 @@ const Wrap = styled.div`
   width: 100%;
 `
 
-Filters.propTypes = {
-  filters: PropTypes.shape({
-    projects: PropTypes.object.isRequired,
-    labels: PropTypes.object.isRequired,
-    milestones: PropTypes.object.isRequired,
-    deadlines: PropTypes.object.isRequired,
-    experiences: PropTypes.object.isRequired,
-    statuses: PropTypes.object.isRequired,
-  }),
-  issues: PropTypes.arrayOf(issueShape).isRequired,
-  bountyIssues: PropTypes.arrayOf(issueShape).isRequired,
-  disableFilter: PropTypes.func.isRequired,
-  disableAllFilters: PropTypes.func.isRequired,
-  style: PropTypes.object,
-}
-
-Filters.defaultProps = {
-  filters: {
-    projects: {},
-    labels: {},
-    milestones: {},
-    deadlines: {},
-    experiences: {},
-    statuses: {},
-  },
-  issues: [],
-  bountyIssues: [],
-}
-
-export default Filters
+export default ActiveFilters
