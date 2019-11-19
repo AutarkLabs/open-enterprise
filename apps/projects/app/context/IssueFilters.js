5597d8c554ade0eba4c53955c5a8a601f689019a
rkzel <rkz@hyperlogos.net>
2019-11-19 21:45:05 +0100

    Filtering Context Provider and helpers

--- apps/projects/app/context/IssueFilters.js
+++ apps/projects/app/context/IssueFilters.js
@@ -1,9 +1,16 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import React, { createContext, useCallback, useContext, useMemo, useState } from 'react'
 import { prepareFilters } from '../components/Shared/FilterBar'
 
 const IssueFiltersContext = createContext()
 
-const INIT_FILTERS = {
+export const sortOptions = [
+  'Newest',
+  'Oldest',
+  'Name ascending',
+  'Name descending',
+]
+
+export const INIT_FILTERS = {
   projects: {},
   labels: {},
   milestones: {},
@@ -19,9 +26,18 @@ export function useIssueFilters() {
     throw new Error('useIssueFilters must be used within a IssueFiltersProvider')
   }
 
-  const { activeFilters, setActiveFilters, availableFilters, setAvailableFilters, activeFiltersCount, setActiveFiltersCount } = context
+  const {
+    activeFilters,
+    setActiveFilters,
+    availableFilters,
+    setAvailableFilters,
+    textFilter,
+    setTextFilter,
+    sortBy,
+    setSortBy,activeFiltersCount, setActiveFiltersCount
+  } = context
 
-  const resetFilters = () => {
+  const resetFilters = useCallback(() => {
     const filters = { ...activeFilters }
 
     // setActiveFilters(INIT_FILTERS) is not working - why?
@@ -33,17 +49,15 @@ export function useIssueFilters() {
 
     setActiveFiltersCount(0)
     setActiveFilters(filters)
-  }
+  }, [activeFilters])
 
-  const buildAvailableFilters = (issues, bountyIssues) => {
-    useEffect(() => {
-      const data = prepareFilters(issues, bountyIssues)
-      setAvailableFilters(data)
-    }, [ issues, bountyIssues ])
-  }
+  const buildAvailableFilters = useCallback((issues, bountyIssues) => {
+    setAvailableFilters(
+      prepareFilters(issues, bountyIssues)
+    )
+  })
 
-
-  const toggleFilter = (type, id) => {
+  const toggleFilter = useCallback((type, id) => {
     const filters = { ...activeFilters }
     if (id in filters[type]) {
       delete filters[type][id]
@@ -53,30 +67,46 @@ export function useIssueFilters() {
       setActiveFiltersCount(activeFiltersCount + 1)
     }
     setActiveFilters(filters)
-  }
+  }, [activeFilters])
 
-  return {
-    buildAvailableFilters,
-    availableFilters,
-    setAvailableFilters,
-    activeFilters,
-    toggleFilter,
-    activeFiltersCount,
-    resetFilters,
-  }
-}
+  const updateTextFilter = e => setTextFilter(e.target.value)
 
-export function IssueFiltersProvider(props) {
-  const [ availableFilters, setAvailableFilters ] = useState(INIT_FILTERS)
-  const [ activeFilters, setActiveFilters ] = useState(INIT_FILTERS)
-  const [ activeFiltersCount, setActiveFiltersCount ] = useState(0)
-
-  return <IssueFiltersContext.Provider value={{
+  const hookHelpers = useMemo(() => ({
     availableFilters,
     setAvailableFilters,
     activeFilters,
     setActiveFilters,
     activeFiltersCount,
     setActiveFiltersCount,
-  }} {...props} />
+    buildAvailableFilters,
+    toggleFilter,
+    resetFilters,
+    textFilter,
+    updateTextFilter,
+    sortBy,
+    setSortBy,
+  }), [ availableFilters, activeFilters, activeFiltersCount, sortBy, textFilter ])
+
+  return hookHelpers
+}
+
+export function IssueFiltersProvider(props) {
+  const [ availableFilters, setAvailableFilters ] = useState(INIT_FILTERS)
+  const [ activeFilters, setActiveFilters ] = useState(INIT_FILTERS)
+  const [ textFilter, setTextFilter ] = useState('')
+  const [ sortBy, setSortBy ] = useState(sortOptions[0])
+  const [ activeFiltersCount, setActiveFiltersCount ] = useState(0)
+
+  const context = useMemo(() => ({
+    availableFilters,
+    setAvailableFilters,
+    activeFilters,
+    setActiveFilters,
+    textFilter,
+    setTextFilter,
+    sortBy,
+    setSortBy,activeFiltersCount, setActiveFiltersCount
+  }), [ availableFilters, activeFilters, activeFiltersCount, textFilter, sortBy ])
+
+  return <IssueFiltersContext.Provider value={context} {...props} />
 }
