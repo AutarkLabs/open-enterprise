ecf695a048340ad0ed362ee29847653b523d009a
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-11-26 08:56:31 -0500

    fix discussion event polling

--- apps/discussions/app/modules/useHandshake.js
+++ apps/discussions/app/modules/useHandshake.js
@@ -2,18 +2,8 @@ import { useEffect, useState } from 'react'
 
 export default () => {
   const [handshakeOccured, setHandshakeOccured] = useState(false)
-  const sendMessageToWrapper = (name, value) => {
-    window.parent.postMessage({ from: 'app', name, value }, '*')
-  }
-  const handleWrapperMessage = ({ data }) => {
-    if (data.from !== 'wrapper') {
-      return
-    }
-    if (data.name === 'ready') {
-      sendMessageToWrapper('ready', true)
-      setHandshakeOccured(true)
-    }
-  }
+
+  const handleWrapperMessage = () => setHandshakeOccured(true)
   useEffect(() => {
     return window.addEventListener('message', handleWrapperMessage)
   }, [])
