fd644284ef219ea5dd14c81710286046f22f309a
Otto G <ottodevs@gmail.com>
2019-11-27 22:46:12 +0100

    Minor improvements and simplification to test script

--- templates/open-enterprise/temp/scripts/test-ganache.sh
+++ templates/open-enterprise/temp/scripts/test-ganache.sh
@@ -17,54 +17,43 @@ cleanup() {
 }
 
 setup_coverage_variables() {
-  PORT=${PORT-8555}
+  ACCOUNTS=${ACCOUNTS-200}  
   BALANCE=${BALANCE-100000}
+  COMMAND=${COMMAND-testrpc-sc}
+  ENVIRONMENT=${ENVIRONMENT-coverage}
   GAS_LIMIT=${GAS_LIMIT-0xfffffffffff}
+  MAIN_TASK=${MAIN_TASK-run_coverage}
   NETWORK_ID=${NETWORK_ID-16}
+  PORT=${PORT-8555}
 }
 
 setup_testing_variables() {
-  PORT=${PORT-8545}
-  BALANCE=${BALANCE-100000}
-  GAS_LIMIT=${GAS_LIMIT-8000000}
-  NETWORK_ID=${NETWORK_ID-15}
   ACCOUNTS=${ACCOUNTS-200}
+  BALANCE=${BALANCE-100000}
+  COMMAND=${COMMAND-ganache-cli}
+  ENVIRONMENT=${ENVIRONMENT-rpc}
+  GAS_LIMIT=${GAS_LIMIT-8000000}
+  MAIN_TASK=${MAIN_TASK-run_tests}
+  NETWORK_ID=${NETWORK_ID-15}
+  PORT=${PORT-8545}
 }
 
-start_ganache() {
-  echo "Starting ganache-cli..."
-  ganache-cli -i ${NETWORK_ID} -l ${GAS_LIMIT} -a ${ACCOUNTS} -e ${BALANCE} -p ${PORT} > /dev/null &
+start_chain() {
+  echo "Starting ${COMMAND}..."
+  ${COMMAND} -i ${NETWORK_ID} -l ${GAS_LIMIT} -a ${ACCOUNTS} -e ${BALANCE} -p ${PORT} > /dev/null &
   pid=$!
-  sleep 3
+  sleep 3 # give time to init chain
   echo "Running ganache-cli with pid ${pid} in port ${PORT}, gas limit set to: ${GAS_LIMIT}"
-}
-
-start_testrpc() {
-  echo "Starting testrpc-sc..."
-  testrpc-sc -i ${NETWORK_ID} -l ${GAS_LIMIT} -e ${BALANCE} -p ${PORT} > /dev/null &
-  rpc_pid=$!
-  sleep 3
-  echo "Running testrpc-sc with pid ${rpc_pid} in port ${PORT}"
 }
 
 deploy_standard_bounties() {
   echo "Deploying bounties contract..."
-  BOUNTIES=$(npm run deploy:bounties | tail -n 1)
+  BOUNTIES=$(npm run deploy:bounties:${ENVIRONMENT} | tail -n 1)
 }
 
-deploy_standard_bounties_coverage() {
-  echo "Deploying bounties contract..."
-  BOUNTIES=$(npm run deploy:bounties:coverage | tail -n 1)
-}
-
-deploy_template_rpc() {
-  echo "Deploying template..."
-  npm run deploy:rpc -- --s ${BOUNTIES}
-}
-
-deploy_template_coverage() {
-  echo "Deploying template..."
-  npm run deploy:coverage -- --s ${BOUNTIES}
+deploy_template() {
+  echo "Deploying template for ${ENVIRONMENT}"
+  npm run deploy:${ENVIRONMENT} -- --s ${BOUNTIES}
 }
 
 clean_deploy() {
@@ -76,25 +65,18 @@ run_tests() {
   truffle test --network rpc $@
 }
 
-measure_coverage() {
-  echo "Measuring coverage..."
+run_coverage() {
+  echo "Measuring coverage $@..."
   # TODO: rimraf or crossenv
+  # It is needed to remove this folder to prevent wrong instrumentation
+  # I didn't find a way to tell solidity-coverage to ignore it
   rm -rf flattened_contracts
-  solidity-coverage
+  solidity-coverage $@
 }
 
-if [ "$SOLIDITY_COVERAGE" = true ]; then
-  setup_coverage_variables
-  start_testrpc
-  clean_deploy
-  deploy_standard_bounties_coverage
-  deploy_template_coverage
-  measure_coverage
-else
-  setup_testing_variables
-  start_ganache
-  clean_deploy
-  deploy_standard_bounties
-  deploy_template_rpc
-  run_tests $@
-fi
+[[ "$SOLIDITY_COVERAGE" = true ]] && setup_coverage_variables || setup_testing_variables
+start_chain
+clean_deploy
+deploy_standard_bounties
+deploy_template
+${MAIN_TASK} $@
