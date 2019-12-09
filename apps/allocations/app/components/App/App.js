ec501f82328cdfba6fd94ab5aded29b10f5d9e3e
Chad Ostrowski <chad@autark.xyz>
2019-12-09 09:24:39 -0500

    Allocations: rename Budgets to Overview
    * Move Header inside
    * Move Allocations list inside
    
    These are not things we would want to appear on the BudgetDetail page

--- apps/allocations/app/components/App/App.js
+++ apps/allocations/app/components/App/App.js
@@ -1,43 +1,22 @@
 import React from 'react'
 import { Route } from 'react-router'
 import { useAragonApi } from '../../api-react'
-import { Button, Header, IconPlus, Main, SidePanel, SyncIndicator } from '@aragon/ui'
+import { Main, SidePanel, SyncIndicator } from '@aragon/ui'
 
 import { IdentityProvider } from '../LocalIdentityBadge/IdentityManager'
-import { Empty } from '../Card'
-import { NewBudget } from '../Panel'
-import { AllocationsHistory, BudgetDetail, Budgets } from '.'
+import { BudgetDetail, Overview } from '.'
 import { usePanel } from '../../context/Panel'
 
 const App = () => {
   const { api, appState } = useAragonApi()
-  const { allocations = [], budgets = [], isSyncing = true } = appState
+  const { isSyncing = true } = appState
 
   const { panel, panelOpen, setPanel } = usePanel()
-
-  const saveBudget = ({ amount, name, token }) => {
-    api
-      .newAccount(
-        name,             // _metadata
-        token.address,    // _token
-        true,             // hasBudget
-        amount
-      )
-      .toPromise()
-    setPanel(null)
-  }
 
   // TODO: Fix this
   // eslint-disable-next-line
   const onExecutePayout = (accountId, payoutId) => {
     api.runPayout(accountId, payoutId).toPromise()
-  }
-
-  const onNewBudget = () => {
-    setPanel({
-      content: NewBudget,
-      data: { heading: 'New budget', saveBudget },
-    })
   }
 
   const handleResolveLocalIdentity = address =>
@@ -54,28 +33,9 @@ const App = () => {
         onResolve={handleResolveLocalIdentity}
         onShowLocalIdentityModal={handleShowLocalIdentityModal}
       >
-        {budgets.length === 0
-          ? <Empty action={onNewBudget} isSyncing={isSyncing} />
-          : (
-            <React.Fragment>
-              <Header
-                primary="Allocations"
-                secondary={
-                  <Button
-                    mode="strong"
-                    icon={<IconPlus />}
-                    onClick={onNewBudget}
-                    label="New budget"
-                  />
-                }
-              />
-              <Route path="/" exact component={Budgets} />
-              <Route path="/budgets/:id" exact component={BudgetDetail} />
-              <SyncIndicator visible={isSyncing} />
-            </React.Fragment>
-          )
-        }
-        { !!allocations.length && <AllocationsHistory allocations={allocations} /> }
+        <Route path="/" exact component={Overview} />
+        <Route path="/budgets/:id" exact component={BudgetDetail} />
+        <SyncIndicator visible={isSyncing} />
         <SidePanel
           title={(panel && panel.data.heading) || ''}
           opened={panelOpen}
--- apps/allocations/app/components/App/index.js
+++ apps/allocations/app/components/App/index.js
@@ -1,3 +1,3 @@
-export { default as Budgets } from './Budgets'
+export { default as Overview } from './Overview'
 export { default as AllocationsHistory } from './AllocationsHistory'
 export { default as BudgetDetail } from './BudgetDetail'
