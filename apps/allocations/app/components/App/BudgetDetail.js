d74d965247cd08f82809a14ac21f0c0c21f6f421
Chad Ostrowski <chad@autark.xyz>
2019-12-11 15:06:28 -0500

    Allocations: create new usePeriod hook
    This checks appState.period, and increments the dates up to the current
    date, if they're not already

--- apps/allocations/app/components/App/BudgetDetail.js
+++ apps/allocations/app/components/App/BudgetDetail.js
@@ -102,10 +102,29 @@ const usePatientRequestPath = () => {
   }
 }
 
+function usePeriod() {
+  const { appState } = useAragonApi()
+  if (!appState.period) return {}
+
+  let { startDate, endDate } = appState.period
+  const duration = endDate - startDate
+
+  // if user visits page from the timeframe of what should be the *next*
+  // period, but the period has not yet been updated on-chain, show them the
+  // period as it will be calculated for their date
+  while (endDate < new Date()) {
+    startDate = new Date(endDate.getTime() + 1000)
+    endDate = new Date(startDate.getTime() + duration)
+  }
+
+  return { startDate, endDate }
+}
+
 export default function BudgetDetail() {
   const { appState } = useAragonApi()
   const [ path, requestPath ] = usePath()
   const { newAllocation } = usePanel()
+  const period = usePeriod()
   const patientlyRequestPath = usePatientRequestPath()
 
   const matchData = path.match(ID_REGEX)
@@ -123,7 +142,6 @@ export default function BudgetDetail() {
   }
 
   const allocations = (appState.allocations || []).filter(a => a.accountId === id)
-  const period = appState.period
   const utilized = budget.amount - budget.remaining
 
   return (
