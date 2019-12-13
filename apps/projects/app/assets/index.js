6fa0c2f3f5092133f8de2cc015f15a3c0c51583d
rkzel <34452131+rkzel@users.noreply.github.com>
2019-12-13 19:03:20 +0100

    Issue Card icons changed (#1757)
    Icons and colors updated in Issue Card (displayed in Issues Tab).

--- apps/projects/app/assets/index.js
+++ apps/projects/app/assets/index.js
@@ -3,11 +3,10 @@ import IconSort from './IconSort'
 import IconGrid from './IconGrid'
 import IconCoins from './IconCoins'
 import IconFilter from './IconFilter'
-import IconBarbell from './IconBarbell'
 import IconOpen from './IconOpen'
 import IconCollapse from './IconCollapse'
 import IconDropArrow from './IconDropArrow'
 import IconDoubleCheck from './IconDoubleCheck'
 import IconUserCheck from './IconUserCheck'
 
-export { IconMore, IconSort, IconGrid, IconCoins, IconFilter, IconBarbell, IconOpen, IconCollapse, IconDropArrow, IconDoubleCheck, IconUserCheck }
+export { IconMore, IconSort, IconGrid, IconCoins, IconFilter, IconOpen, IconCollapse, IconDropArrow, IconDoubleCheck, IconUserCheck }
