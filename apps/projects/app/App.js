71b147e948003eeea82434ed35edbebd37ebf7cb
PeterMPhillips <github@pmphillips.ca>
2020-01-24 11:17:31 -0800

    fix(projects): use main component with emptywrapper

--- apps/projects/app/App.js
+++ apps/projects/app/App.js
@@ -101,9 +101,11 @@ const App = () => {
   const noop = () => {}
   if (githubLoading) {
     return (
-      <EmptyWrapper>
-        <LoadingAnimation />
-      </EmptyWrapper>
+      <Main theme={appearance}>
+        <EmptyWrapper>
+          <LoadingAnimation />
+        </EmptyWrapper>
+      </Main>
     )
   } else if (github.status === STATUS.INITIAL) {
     return (
