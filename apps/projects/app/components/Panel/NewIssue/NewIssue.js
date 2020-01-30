bee59aa4543bda5053d09fe676d7f6d03d5d7d0d
Kevin Siegler <kevinsiegler54@gmail.com>
2020-01-30 01:05:37 -0500

    chore(projects): integrate `setIssue` panel call

--- apps/projects/app/components/Panel/NewIssue/NewIssue.js
+++ apps/projects/app/components/Panel/NewIssue/NewIssue.js
@@ -9,6 +9,7 @@ import { LoadingAnimation } from '../../Shared'
 import { usePanelManagement } from '../../Panel'
 import { useDecoratedRepos } from '../../../context/DecoratedRepos'
 import AuthorizeGitHub from './AuthorizeGitHub'
+import { ipfsAdd } from '../../../utils/ipfs-helpers'
 
 // TODO: labels
 // TODO: import validator from '../data/validation'
@@ -31,6 +32,7 @@ const Creating = () => (
 class NewIssue extends React.PureComponent {
   state = NewIssue.initialState
   static propTypes = {
+    account: PropTypes.string,
     closePanel: PropTypes.func.isRequired,
     reposManaged: PropTypes.oneOfType([
       PropTypes.string,
@@ -86,7 +88,7 @@ class NewIssue extends React.PureComponent {
 
   render() {
     const { title, description, selectedProject } = this.state
-    const { reposManaged } = this.props
+    const { reposManaged, issues, repoHexIds, api } = this.props
     const {
       projectChange,
       titleChange,
@@ -103,66 +105,113 @@ class NewIssue extends React.PureComponent {
 
     const id = selectedProject > 0 ? reposIds[selectedProject - 1] : ''
 
+    const handleSubmit = async e => {
+      e.preventDefault()
+      const newIssueData = {
+        number: issues.filter(i => i.id === repoHexIds[selectedProject - 1]).length,
+        id: repoHexIds[selectedProject - 1],
+        title,
+        body: description,
+        author: {
+          login: this.props.account,
+        },
+        repository: reposManaged[selectedProject - 1],
+        labels: { totalCount: 0, edges: Array(0) },
+        milestone: null,
+        state: 'OPEN',
+        url: null
+      }
+      const hashedIssueData = await ipfsAdd(newIssueData)
+      api.setIssue(newIssueData.id, newIssueData.number, hashedIssueData).toPromise()
+    }
+
     // TODO: refetch Issues list after mutation
 
     return (
       <div css={`margin: ${2 * GU}px 0`}>
-        <Mutation
-          mutation={NEW_ISSUE}
-          variables={{ title, description, id }}
-          onError={console.error}
-        >
-          {(newIssue, result) => {
-            const { data, loading, error, called } = result
-            if (!called) {
-              return (
-                <Form
-                  onSubmit={newIssue}
-                  submitText="Submit Issue"
-                  submitDisabled={this.canSubmit()}
-                >
-                  <Field label="Project">
-                    <DropDown
-                      items={items}
-                      selected={selectedProject}
-                      onChange={projectChange}
-                      wide
-                      required
-                    />
-                  </Field>
-                  <Field label="Title">
-                    <TextInput onChange={titleChange} required wide />
-                  </Field>
-                  <Field label="Description">
-                    <TextInput.Multiline
-                      rows={3}
-                      style={{
-                        resize: 'none',
-                        height: 'auto',
-                        paddingTop: '5px',
-                        paddingBottom: '5px',
-                      }}
-                      onChange={descriptionChange}
-                      wide
-                    />
-                  </Field>
-                </Form>
-              )
-            } // end if(!called)
-            if (loading) {
-              return <Creating />
-            }
-            if (error) {
-              return <div>Error</div>
-            }
+        <Field label="Project">
+          <DropDown
+            items={items}
+            selected={selectedProject}
+            onChange={projectChange}
+            wide
+            required
+          />
+        </Field>
+        {selectedProject > 0 && (reposManaged[selectedProject - 1].decoupled ? (
+          /* Repo is Decoupled */
+          <Form
+            onSubmit={handleSubmit}
+            submitText="Submit Issue"
+            submitDisabled={this.canSubmit()}
+          >
+            <Field label="Title">
+              <TextInput onChange={titleChange} required wide />
+            </Field>
+            <Field label="Description">
+              <TextInput.Multiline
+                rows={3}
+                style={{
+                  resize: 'none',
+                  height: 'auto',
+                  paddingTop: '5px',
+                  paddingBottom: '5px',
+                }}
+                onChange={descriptionChange}
+                wide
+              />
+            </Field>
+          </Form>
+        ) : ( 
+          /* repo is connected to github */
+          <Mutation
+            mutation={NEW_ISSUE}
+            variables={{ title, description, id }}
+            onError={console.error}
+          >
+            {(newIssue, result) => {
+              const { data, loading, error, called } = result
+              if (!called) {
+                return (
+                  <Form
+                    onSubmit={newIssue}
+                    submitText="Submit Issue"
+                    submitDisabled={this.canSubmit()}
+                  >
+                    <Field label="Title">
+                      <TextInput onChange={titleChange} required wide />
+                    </Field>
+                    <Field label="Description">
+                      <TextInput.Multiline
+                        rows={3}
+                        style={{
+                          resize: 'none',
+                          height: 'auto',
+                          paddingTop: '5px',
+                          paddingBottom: '5px',
+                        }}
+                        onChange={descriptionChange}
+                        wide
+                      />
+                    </Field>
+                  </Form>
+                )
+              } // end if(!called)
+              if (loading) {
+                return <Creating />
+              }
+              if (error) {
+                return <div>Error</div>
+              }
 
-            const { createIssue } = data
-            if (createIssue) {
-              this.props.closePanel()
-            }
-            return null
-          }}
-        </Mutation>
+              const { createIssue } = data
+              if (createIssue) {
+                this.props.closePanel()
+              }
+              return null
+            }}
+          </Mutation>
+        ))}
       </div>
     )
   }
@@ -173,22 +222,29 @@ class NewIssue extends React.PureComponent {
 const NewIssueWrap = () => {
   const { closePanel } = usePanelManagement()
   const repos = useDecoratedRepos()
-  const { appState: { github } } = useAragonApi()
+  const { appState: { github, issues }, api, connectedAccount } = useAragonApi()
   if (!github.scope) return <AuthorizeGitHub />
 
   const repoNames = repos
     ? repos.map(repo => ({
       name: repo.metadata.name,
       id: repo.data._repo,
+      hexId: repo.id,
+      decoupled: repo.decoupled
     }))
     : 'No repos'
   const reposIds = (repos || []).map(repo => repo.data._repo)
+  const repoHexIds = (repos || []).map(repo => repo.id)
 
   return (
     <NewIssue
+      api={api}
+      account={connectedAccount}
+      issues={issues}
       closePanel={closePanel}
       reposManaged={repoNames}
       reposIds={reposIds}
+      repoHexIds={repoHexIds}
     />
   )
 }
